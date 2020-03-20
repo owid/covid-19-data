08c3ca35d3d9e3bc6ea71f4b54f547778fa84840
Daniel Gavrilov <dgavrilov95@gmail.com>
2020-03-20 12:35:24 +0000

    Add ECDC script

---
+++ scripts/ecdc.py
@@ -0,0 +1,107 @@
+import os
+import sys
+import pandas as pd
+
+CURRENT_DIR = os.path.dirname(__file__)
+
+sys.path.append(CURRENT_DIR)
+
+from shared import load_population, inject_total_daily_cols, inject_world, inject_per_million, inject_days_since_all, inject_population, standard_export
+
+INPUT_PATH = os.path.join(CURRENT_DIR, '../input/ecdc/')
+OUTPUT_PATH = os.path.join(CURRENT_DIR, '../public/data/ecdc/')
+TMP_PATH = os.path.join(CURRENT_DIR, '../tmp')
+
+LOCATIONS_CSV_PATH = os.path.join(INPUT_PATH, 'locations.csv')
+DATA_XLS_PATH = os.path.join(INPUT_PATH, 'releases', '2020-03-19.xlsx')
+
+def load_data():
+    return pd.read_excel(
+        DATA_XLS_PATH,
+        # Namibia has 'NA' 2-letter code, we don't want that to be <NA>
+        keep_default_na=False
+    )
+
+def load_locations():
+    return pd.read_csv(
+        LOCATIONS_CSV_PATH,
+        keep_default_na=False,
+        index_col=['GeoId', 'Countries and territories']
+    )
+
+def _load_merged():
+    df_data = load_data()
+    df_locs = load_locations()
+    return df_data.merge(
+        df_locs,
+        how='left',
+        on=['GeoId', 'Countries and territories'],
+    )
+
+def check_data_correctness():
+    errors = 0
+    df_merged = _load_merged()
+    df_uniq = df_merged[['Countries and territories', 'GeoId', 'location']].drop_duplicates()
+    df_pop = load_population()
+    if df_uniq['location'].isnull().any():
+        print("Error: Could not find OWID names for:")
+        print(df_uniq[df_uniq['location'].isnull()])
+        csv_path = os.path.join(TMP_PATH, 'ecdc_locations.csv')
+        print(os.path.abspath(TMP_PATH))
+        os.system('mkdir -p %s' % os.path.abspath(TMP_PATH))
+        df_uniq \
+            .sort_values(by=['location']) \
+            .to_csv(csv_path, index=False)
+        print("Saved CSV file to be standardized at %s" % csv_path)
+        errors += 1
+    if df_merged.duplicated(subset=['DateRep', 'location']).any():
+        print("Found duplicate rows:")
+        print(df_merged[df_merged.duplicated(subset=['DateRep', 'Our World in Data'])])
+        errors += 1
+    pop_entity_diff = set(df_uniq['location']) - set(df_pop['location'])
+    if len(pop_entity_diff) > 0:
+        # this is not an error, so don't increment errors variable
+        print("These entities were not found in the population dataset:")
+        print(pop_entity_diff)
+    return True if errors == 0 else False
+
+# Must output columns:
+# date, location, new_cases, new_deaths, total_cases, total_deaths
+def load_standardized():
+    df = _load_merged() \
+        .drop(columns=[
+            'Countries and territories', 'GeoId',
+            'Day', 'Month', 'Year',
+        ]) \
+        .rename(columns={
+            'DateRep': 'date',
+            'Cases': 'new_cases',
+            'Deaths': 'new_deaths'
+        })
+    df = df[['date', 'location', 'new_cases', 'new_deaths']]
+    df = inject_world(df)
+    df = inject_total_daily_cols(df, ['cases', 'deaths'])
+    df = inject_per_million(df, [
+        'new_cases',
+        'new_deaths',
+        'total_cases',
+        'total_deaths'
+    ])
+    df = inject_days_since_all(df)
+    assert not df.duplicated(subset=['location', 'date']).any()
+    return df.sort_values(by=['location', 'date'])
+
+def export():
+    # locations.csv
+    df_loc = inject_population(load_locations().reset_index())
+    df_loc['population_year'] = df_loc['population_year'].round().astype('Int64')
+    df_loc['population'] = df_loc['population'].round().astype('Int64')
+    df_loc.to_csv(os.path.join(OUTPUT_PATH, 'locations.csv'), index=False)
+    # The rest of the CSVs
+    return standard_export(load_standardized(), OUTPUT_PATH)
+
+if __name__ == '__main__':
+    if export():
+        print("Successfully exported CSVs for ECDC.")
+    else:
+        print("ECDC Export failed.")
---
+++ scripts/shared.py
@@ -0,0 +1,179 @@
+import pandas as pd
+import os
+from datetime import datetime
+
+CURRENT_DIR = os.path.dirname(__file__)
+POPULATION_CSV_PATH = os.path.join(CURRENT_DIR, '../input/un/population_thousands.csv')
+
+# Per population calculations
+
+def _find_closest_year_row(df, year=2020):
+    """Returns the row which is closest to the year specified (in either direction)"""
+    df = df.copy()
+    df['year'] = df['year'].sort_values(ascending=True)
+    return df.loc[df['year'].map(lambda x: abs(x - 2020)).idxmin()]
+
+def load_population(year=2020):
+    df = pd.read_csv(
+        POPULATION_CSV_PATH,
+        keep_default_na=False
+    )
+    df['population'] = df['population_thousands'] * 1000
+    return pd.DataFrame([
+        _find_closest_year_row(df_group, year)
+        for loc, df_group in df.groupby('entity')
+    ]) \
+    .dropna() \
+    .drop(columns=['population_thousands']) \
+    .rename(columns={'entity': 'location', 'year': 'population_year'})
+
+# Useful for adding it to regions.csv and
+def inject_population(df):
+    return df.merge(
+        load_population(),
+        how='left',
+        on='location'
+    )
+
+def inject_per_million(df, measures):
+    df = inject_population(df)
+    for measure in measures:
+        pop_measure = measure + '_per_million'
+        df[pop_measure] = df[measure] / (df['population'] / 1e6)
+    return df.drop(columns=['population_year', 'population'])
+
+# "World" aggregate
+
+def inject_world(df):
+    df_global = df.groupby('date').sum().reset_index()
+    df_global['location'] = 'World'
+    return pd.concat([df, df_global], sort=True, ignore_index=True)
+
+# Total/daily calculation
+
+def inject_total_daily_cols(df, measures):
+    # must sort in order to have the cumsum() and diff() in the right direction
+    df = df.copy().sort_values(by=['location', 'date'])
+    for measure in measures:
+        total_col = 'total_%s' % measure
+        daily_col = 'new_%s' % measure
+        if total_col not in df.columns and daily_col in df.columns:
+            df[total_col] = df.groupby('location')[daily_col].cumsum().astype('Int64')
+        elif daily_col not in df.columns and total_col in df.columns:
+            df[daily_col] = df.groupby('location')[total_col].diff().astype('Int64')
+    return df
+
+
+days_since_spec = {
+    'days_since_100th_case': {
+        'col': 'total_cases',
+        'threshold': 100
+    },
+    'days_since_10th_death': {
+        'col': 'total_deaths',
+        'threshold': 10
+    },
+    'days_since_1_per_million_cases': {
+        'col': 'total_cases_per_million',
+        'threshold': 1
+    },
+    'days_since_0_1_per_million_deaths': {
+        'col': 'total_deaths_per_million',
+        'threshold': 0.1
+    }
+}
+
+def _get_date_of_nth(df, col, nth):
+    try:
+        df_gt_nth = df[df[col] >= nth]
+        earliest = df.loc[pd.to_datetime(df_gt_nth['date']).idxmin()]
+        return earliest['date']
+    except:
+        return None
+
+def _positive_date_diff(a, b):
+    diff = (a - b).days
+    return diff if diff >= 0 else None
+
+def _inject_days_since(df, col, ref_date):
+    df[col] = df['date'].map(lambda date: _positive_date_diff(pd.to_datetime(date), pd.to_datetime(ref_date))).astype('Int64')
+    df = df
+    return df
+
+def _inject_days_since_group(df):
+    df = df.copy()
+    for col, spec in days_since_spec.items():
+        date = _get_date_of_nth(df, spec['col'], spec['threshold'])
+        if date is not None:
+            df = _inject_days_since(df, col, date)
+    return df
+
+def inject_days_since_all(df):
+    return pd.concat([
+        _inject_days_since_group(df_group)
+        for loc, df_group in df.groupby('location')
+    ])
+
+
+# Export logic
+
+KEYS = ['date', 'location']
+
+BASE_MEASURES = [
+    'new_cases', 'new_deaths',
+    'total_cases', 'total_deaths'
+]
+
+PER_MILLION_MEASURES = [
+    '%s_per_million' % m for m in BASE_MEASURES
+]
+
+DAYS_SINCE_MEASURES = list(days_since_spec.keys())
+
+# Should keep these append-only in case someone external depends on the order
+FULL_DATA_COLS = [
+    *KEYS,
+    *BASE_MEASURES
+]
+
+GRAPHER_COL_NAMES = {
+    'location': 'Country',
+    'date': 'Year',
+    'new_cases': 'Daily new confirmed cases of COVID-19',
+    'new_deaths': 'Daily new confirmed deaths due to COVID-19',
+    'total_cases': 'Total confirmed cases of COVID-19',
+    'total_deaths': 'Total confirmed deaths due to COVID-19',
+    'new_cases_per_million': 'Daily new confirmed cases of COVID-19 per million people',
+    'new_deaths_per_million': 'Daily new confirmed deaths due to COVID-19 per million people',
+    'total_cases_per_million': 'Total confirmed cases of COVID-19 per million people',
+    'total_deaths_per_million': 'Total confirmed deaths due to COVID-19 per million people',
+    'days_since_100th_case': 'Days since the total confirmed cases of COVID-19 reached 100',
+    'days_since_10th_death': 'Days since the total confirmed deaths of COVID-19 reached 10',
+    'days_since_1_per_million_cases': 'Days since the total confirmed cases of COVID-19 per million people reached 1',
+    'days_since_0_1_per_million_deaths': 'Days since the total confirmed deaths of COVID-19 per million people reached 0.1'
+}
+
+def existsin(l1, l2):
+    return [x for x in l1 if x in l2]
+
+def standard_export(df, output_path):
+    # full_data.csv
+    full_data_cols = existsin(FULL_DATA_COLS, df.columns)
+    df[full_data_cols].to_csv(
+        os.path.join(output_path, 'full_data.csv'),
+        index=False
+    )
+    # Grapher
+    df_grapher = df.copy()
+    df_grapher['date'] = pd.to_datetime(df_grapher['date']).map(lambda date: (date - datetime(2020, 1, 21)).days)
+    df_grapher = df_grapher[GRAPHER_COL_NAMES.keys()] \
+        .rename(columns=GRAPHER_COL_NAMES) \
+        .to_csv(os.path.join(output_path, 'grapher.csv'), index=False)
+    # Pivot variables (wide format)
+    for col_name in [*BASE_MEASURES, *PER_MILLION_MEASURES]:
+        df_pivot = df.pivot(index='date', columns='location', values=col_name)
+        # move World to first column
+        cols = df_pivot.columns.tolist()
+        cols.insert(0, cols.pop(cols.index('World')))
+        df_pivot[cols].to_csv(os.path.join(output_path, '%s.csv' % col_name))
+    return True
