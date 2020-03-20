7f4be1b968ba7ecd46d67f86ae871ce3418afd5c
Daniel Gavrilov <dgavrilov95@gmail.com>
2020-03-20 15:39:39 +0000

    Automation improvements: auto-download & prompt to pick release

--- scripts/ecdc.py
+++ scripts/ecdc.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pandas as pd
+from datetime import datetime, timedelta
 
 CURRENT_DIR = os.path.dirname(__file__)
 
@@ -12,12 +13,26 @@ INPUT_PATH = os.path.join(CURRENT_DIR, '../input/ecdc/')
 OUTPUT_PATH = os.path.join(CURRENT_DIR, '../public/data/ecdc/')
 TMP_PATH = os.path.join(CURRENT_DIR, '../tmp')
 
-LOCATIONS_CSV_PATH = os.path.join(INPUT_PATH, 'locations.csv')
-DATA_XLS_PATH = os.path.join(INPUT_PATH, 'releases', '2020-03-20-modified.xlsx')
+LOCATIONS_CSV_PATH = os.path.join(INPUT_PATH, 'ecdc_country_standardized.csv')
+RELEASES_PATH = os.path.join(INPUT_PATH, 'releases')
 
-def load_data():
+def download(last_n=2):
+    daterange = pd.date_range(end=datetime.utcnow(), periods=last_n).to_pydatetime().tolist()
+    for date in daterange:
+        filename = date.strftime('%Y-%m-%d')
+        os.system('wget -c -N -o wget.log -O %(DIR)s/%(filename)s.xlsx https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-%(filename)s.xlsx || rm -f %(DIR)s/%(filename)s.xlsx' % {
+            'filename': filename,
+            'DIR': RELEASES_PATH
+        })
+        os.system('wget -c -N -o wget.log -O %(DIR)s/%(filename)s.xls https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-%(filename)s.xls || rm -f %(DIR)s/%(filename)s.xls' % {
+            'filename': filename,
+            'DIR': RELEASES_PATH
+        })
+
+def load_data(filename):
+    filepath = os.path.join(RELEASES_PATH, filename)
     return pd.read_excel(
-        DATA_XLS_PATH,
+        filepath,
         # Namibia has 'NA' 2-letter code, we don't want that to be <NA>
         keep_default_na=False
     )
@@ -25,50 +40,54 @@ def load_data():
 def load_locations():
     return pd.read_csv(
         LOCATIONS_CSV_PATH,
-        keep_default_na=False,
-        index_col=['GeoId', 'Countries and territories']
-    )
+        keep_default_na=False
+    ).rename(columns={
+        'Country': 'Countries and territories',
+        'Our World In Data Name': 'location'
+    })
 
-def _load_merged():
-    df_data = load_data()
+def _load_merged(filename):
+    df_data = load_data(filename)
     df_locs = load_locations()
     return df_data.merge(
         df_locs,
         how='left',
-        on=['GeoId', 'Countries and territories'],
+        on=['Countries and territories']
     )
 
-def check_data_correctness():
+def check_data_correctness(filename):
     errors = 0
-    df_merged = _load_merged()
+    df_merged = _load_merged(filename)
     df_uniq = df_merged[['Countries and territories', 'GeoId', 'location']].drop_duplicates()
-    df_pop = load_population()
     if df_uniq['location'].isnull().any():
         print("Error: Could not find OWID names for:")
         print(df_uniq[df_uniq['location'].isnull()])
-        csv_path = os.path.join(TMP_PATH, 'ecdc_locations.csv')
+        csv_path = os.path.join(TMP_PATH, 'ecdc.csv')
         print(os.path.abspath(TMP_PATH))
         os.system('mkdir -p %s' % os.path.abspath(TMP_PATH))
-        df_uniq \
+        df_uniq[['Countries and territories']] \
+            .rename(columns={'Countries and territories': 'Country'}) \
             .sort_values(by=['location']) \
             .to_csv(csv_path, index=False)
-        print("Saved CSV file to be standardized at %s" % csv_path)
+        print("Saved CSV file to be standardized at %s. Move it to %s once you are done." % (csv_path, LOCATIONS_CSV_PATH))
         errors += 1
     if df_merged.duplicated(subset=['DateRep', 'location']).any():
         print("Found duplicate rows:")
-        print(df_merged[df_merged.duplicated(subset=['DateRep', 'Our World in Data'])])
+        print(df_merged[df_merged.duplicated(subset=['DateRep', 'location'])])
         errors += 1
+    df_pop = load_population()
     pop_entity_diff = set(df_uniq['location']) - set(df_pop['location'])
     if len(pop_entity_diff) > 0:
         # this is not an error, so don't increment errors variable
-        print("These entities were not found in the population dataset:")
+        print("\n[Warning] These entities were not found in the population dataset:")
         print(pop_entity_diff)
+        print()
     return True if errors == 0 else False
 
 # Must output columns:
 # date, location, new_cases, new_deaths, total_cases, total_deaths
-def load_standardized():
-    df = _load_merged() \
+def load_standardized(filename):
+    df = _load_merged(filename) \
         .drop(columns=[
             'Countries and territories', 'GeoId',
             'Day', 'Month', 'Year',
@@ -93,20 +112,47 @@ def load_standardized():
         print("Dataet contains duplicates for date, location:")
         df_dupes = df[df_dupe_mask]
         print(df_dupes)
-        assert False
+        sys.exit(1)
     return df.sort_values(by=['location', 'date'])
 
-def export():
+def export(filename):
     # locations.csv
     df_loc = inject_population(load_locations().reset_index())
     df_loc['population_year'] = df_loc['population_year'].round().astype('Int64')
     df_loc['population'] = df_loc['population'].round().astype('Int64')
     df_loc.to_csv(os.path.join(OUTPUT_PATH, 'locations.csv'), index=False)
     # The rest of the CSVs
-    return standard_export(load_standardized(), OUTPUT_PATH)
+    return standard_export(load_standardized(filename), OUTPUT_PATH)
 
 if __name__ == '__main__':
-    if export():
-        print("Successfully exported CSVs for ECDC.")
+    import inquirer
+    from glob import glob
+
+    print("\nAttempting to download latest report...")
+    download(last_n=2)
+
+    print("\nIf you don't see the latest report in the options below, please download it from: \nhttps://www.ecdc.europa.eu/en/publications-data/download-todays-data-geographic-distribution-covid-19-cases-worldwide\n")
+
+    filenames = glob(os.path.join(RELEASES_PATH, '*.xlsx'))
+    filenames.extend(glob(os.path.join(RELEASES_PATH, '*.xls')))
+    filenames = list(map(os.path.basename, sorted(filenames, reverse=True)))
+
+    answers = inquirer.prompt([
+        inquirer.List('filename',
+                  message='Which release to use?',
+                  choices=filenames,
+                  default=0)
+    ])
+
+    filename = answers['filename']
+
+    if check_data_correctness(filename):
+        print("Data correctness check passed.\n")
     else:
-        print("ECDC Export failed.")
+        print("Data correctness check failed.\n")
+        sys.exit(1)
+
+    if export(filename):
+        print("Successfully exported CSVs to %s\n" % os.path.abspath(OUTPUT_PATH))
+    else:
+        print("ECDC Export failed.\n")
