40f5758d5bf35d61b6f614b4d68b96d05aff3685
Daniel Gavrilov <dgavrilov95@gmail.com>
2020-04-17 12:15:57 +0100

    Change megafile paths to be relative to current file

--- scripts/scripts/megafile.py
+++ scripts/scripts/megafile.py
@@ -6,9 +6,13 @@ Merges the main COVID-19 testing dataset with each of the COVID-19 ECDC datasets
 - Includes country ISO codes in a column next to country names.
 """
 
-
+import os
 from functools import reduce
 import pandas as pd
+
+CURRENT_DIR = os.path.dirname(__file__)
+INPUT_DIR = os.path.join(CURRENT_DIR, '../input/')
+DATA_DIR = os.path.join(CURRENT_DIR, '../../public/data/')
 
 
 def get_testing():
@@ -21,7 +25,7 @@ def get_testing():
     	testing {dataframe}
     """
 
-    testing = pd.read_csv("../../public/data/testing/covid-testing-all-observations.csv", usecols=[
+    testing = pd.read_csv(os.path.join(DATA_DIR, "testing/covid-testing-all-observations.csv"), usecols=[
         "Entity",
         "Date",
         "Cumulative total",
@@ -92,7 +96,7 @@ def get_ecdc():
     # Process each file and melt it to vertical format
     for ecdc_var in ecdc_variables:
 
-        tmp = pd.read_csv("../../public/data/ecdc/{}.csv".format(ecdc_var))
+        tmp = pd.read_csv(os.path.join(DATA_DIR, "../../public/data/ecdc/{}.csv".format(ecdc_var)))
         tmp = tmp.drop(columns=["World", "International"])
         country_cols = list(tmp.columns)
         country_cols.remove("date")
@@ -133,7 +137,7 @@ def generate_megafile():
         .sort_values(["location", "date"])
     )
 
-    iso_codes = pd.read_csv("../input/iso/iso3166_1_alpha_3_codes.csv")
+    iso_codes = pd.read_csv(os.path.join(INPUT_DIR, "iso/iso3166_1_alpha_3_codes.csv"))
 
     missing_iso = set(all_covid.location).difference(set(iso_codes.location))
     if len(missing_iso) > 0:
@@ -146,8 +150,8 @@ def generate_megafile():
     integer_vars = ["total_cases", "new_cases", "total_deaths", "new_deaths"]
     all_covid[integer_vars] = all_covid[integer_vars].fillna(0).astype(int)
 
-    all_covid.to_csv("../../public/data/owid-covid-data.csv", index=False)
-    all_covid.to_excel("../../public/data/owid-covid-data.xlsx", index=False)
+    all_covid.to_csv(os.path.join(DATA_DIR, "owid-covid-data.csv"), index=False)
+    all_covid.to_excel(os.path.join(DATA_DIR, "owid-covid-data.xlsx"), index=False)
 
 
 if __name__ == '__main__':
