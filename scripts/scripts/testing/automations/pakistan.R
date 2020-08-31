e44e364c41ce08dcb3d1059924f1b1e52a7597c9
Edouard Mathieu <edmat@pm.me>
2020-08-31 15:08:16 +0200

    Update pakistan.R

--- scripts/scripts/testing/automations/pakistan.R
+++ scripts/scripts/testing/automations/pakistan.R
@@ -1,15 +1,21 @@
 url <- "http://www.covid.gov.pk/"
+page <- read_html(url)
 
-count <- read_html(url) %>%
-    html_nodes(".covid-statistics .container .row .text-center") %>%
-    html_text()
-
-count <- count[str_detect(str_to_lower(count), "total tests")] %>%
+count <- page %>%
+    html_nodes(".top-statistics .active .counter") %>%
+    html_text() %>%
     str_replace_all("[^\\d]", "") %>%
     as.integer()
 
+date <- page %>%
+    html_node(".update-time") %>%
+    html_text() %>%
+    str_extract("^.*2020") %>%
+    dmy()
+
 add_snapshot(
     count = count,
+    date = date,
     sheet_name = "Pakistan",
     country = "Pakistan",
     units = "tests performed",
