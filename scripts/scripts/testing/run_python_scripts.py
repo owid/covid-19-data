4a79d1198f2ba571dbe714993183b8aa35da0b76
Edouard Mathieu <edmat@pm.me>
2020-10-01 11:38:40 +0200

    Update run_python_scripts.py

--- scripts/scripts/testing/run_python_scripts.py
+++ scripts/scripts/testing/run_python_scripts.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import re
 from glob import glob
 import datetime
 
@@ -10,7 +11,11 @@ scripts_path = "automations/incremental/*.py" if execution_mode == "quick" else 
 
 if len(SKIP) > 0:
     print(f"Warning message:\nSkipping the following countries: {', '.join(SKIP)}")
+    SKIP = "|".join(SKIP)
 
-for file in glob(scripts_path):
-    print(f"{datetime.datetime.now().replace(microsecond=0)} - {file}")
-    os.system(f"python3 {file}")
+scripts = glob(scripts_path)
+scripts = [s for s in scripts if not bool(re.search(pattern=SKIP, string=s))]
+
+for script_name in scripts:
+    print(f"{datetime.datetime.now().replace(microsecond=0)} - {script_name}")
+    os.system(f"python3 {script_name}")
