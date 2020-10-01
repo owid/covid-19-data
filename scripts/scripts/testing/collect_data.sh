e04cac5f869d2f7433fe8424f76a8fc9eebffe8c
Edouard Mathieu <edmat@pm.me>
2020-10-01 11:39:06 +0200

    Simplify bash script

--- scripts/scripts/testing/collect_data.sh
+++ scripts/scripts/testing/collect_data.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
-
 set -e
 
-for arg in "$@"
-do
-    case $arg in
-        -m=*|--mode=*)
-        MODE="${arg#*=}"
-        shift
-        ;;
-    esac
-done
-
-if [ "$MODE" != "quick" ] && [ "$MODE" != "update" ]; then
+if [ $1 != quick ] && [ $1 != update ]; then
     echo "Wrong execution mode ('quick' or 'update')."
     exit
 fi
@@ -21,5 +10,5 @@ cd ~/Git/covid-19-data
 git pull
 
 cd ~/Git/covid-19-data/scripts/scripts/testing
-python3 run_python_scripts.py "$MODE"
-Rscript run_r_scripts.R "$MODE"
+python3 run_python_scripts.py $1
+Rscript run_r_scripts.R $1
