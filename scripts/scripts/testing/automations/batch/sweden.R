e1b1903bd9370b50571d59ef788b87e92b7c2517
Edouard Mathieu <edmat@pm.me>
2020-10-09 16:50:30 +0200

    Fix collection for Sweden

--- scripts/scripts/testing/automations/batch/sweden.R
+++ scripts/scripts/testing/automations/batch/sweden.R
@@ -1,18 +1,17 @@
-url <- "https://www.folkhalsomyndigheten.se/smittskydd-beredskap/utbrott/aktuella-utbrott/covid-19/antal-individer-som-har-testats-for-covid-19/"
+url <- "https://www.folkhalsomyndigheten.se/smittskydd-beredskap/utbrott/aktuella-utbrott/covid-19/statistik-och-analyser/antalet-testade-for-covid-19/"
 page <- read_html(url)
 
 df <- page %>%
     html_node("#content-primary table") %>%
     html_table() %>%
     data.table()
-df[, Nukleinsyrapåvisning := as.integer(str_replace_all(Nukleinsyrapåvisning, "\\s", ""))]
+df[, Weekly := as.integer(str_replace_all(`NukleinsyrapåvisningGenomförda tester`, "\\s", ""))]
 
 df <- df[str_detect(Vecka, "vecka")]
 df[, Vecka := as.integer(str_replace(Vecka, "vecka ", ""))]
 df[, Date := ymd("2019-12-29") + 7 * Vecka]
 
-setnames(df, "Nukleinsyrapåvisning", "Weekly")
-df[, c("Antikroppspåvisning", "Vecka") := NULL]
+df <- df[, c("Date", "Weekly")]
 setorder(df, Date)
 
 split_week <- function(row_idx) {
@@ -20,12 +19,10 @@ split_week <- function(row_idx) {
     weekly_tests <- df[row_idx, Weekly]
     start_date <- end_date - 6
     daily_tests <- round(weekly_tests / 7)
-    notes <- sprintf("Dividing %s (weekly total) by 7", format(weekly_tests, big.mark = ","))
     dates <- seq.Date(start_date, end_date, by = "1 day")
     return(data.table(
         Date = dates,
-        `Daily change in cumulative total` = daily_tests,
-        Notes = notes
+        `Daily change in cumulative total` = daily_tests
     ))
 }
 
@@ -33,10 +30,17 @@ df <- rbindlist(lapply(1:nrow(df), FUN = split_week))
 
 df[, `Source URL` := url]
 df[, Country := "Sweden"]
-df[, Units := "samples tested"]
+df[, Units := "tests performed"]
 df[, `Source label` := "Swedish Public Health Agency"]
 df[, Notes := NA]
 df[, `Testing type` := "PCR only"]
 df[, `Cumulative total` := NA_integer_]
 
-fwrite(df, "automated_sheets/Sweden - samples tested.csv")
+existing <- fread("automated_sheets/Sweden - tests performed.csv")
+existing[, Date := ymd(Date)]
+
+df <- rbindlist(list(existing, df), use.names = TRUE)
+setorder(df, -Date)
+df <- unique(df)
+
+fwrite(df, "automated_sheets/Sweden - tests performed.csv")
