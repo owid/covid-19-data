d4c061f204da193a38648853a5c59d90f91a64d6
Edouard Mathieu <edmat@pm.me>
2020-10-12 17:37:46 +0200

    Update auto_quick_collect.sh

--- scripts/scripts/testing/auto_quick_collect.sh
+++ scripts/scripts/testing/auto_quick_collect.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-set -e
+
+USER_EMAIL=
 
 cd ~/covid-19-data/scripts/scripts/testing
 git stash push -- automated_sheets/*
 git pull
-bash collect_data.sh quick
+bash collect_data.sh quick &> auto_quick_collect.log
+cat auto_quick_collect.log | mail -s "Automated testing collection - `date`" $USER_EMAIL
 git add automated_sheets/*
 git commit -m "Automated testing collection"
 git push
