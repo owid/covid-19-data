99c09ba9a15bec57e5deb021bbcc6562d90d060d
Edouard Mathieu <edmat@pm.me>
2020-10-13 14:02:45 +0200

    Add tests performed series for France

--- scripts/scripts/testing/automations/batch/france.py
+++ scripts/scripts/testing/automations/batch/france.py
@@ -3,7 +3,27 @@ import pandas as pd
 import numpy as np
 
 
-def main():
+def tests_performed():
+
+    df = pd.read_excel("input/data.xlsx", sheet_name ="Graphiques", skiprows=3)
+    df = df.iloc[0:df[df.Indicateurs.isna()].index.min(), :]
+
+    df["Valeur"] = df["Valeur"].str.replace(r"\s", "").astype(int)
+
+    df = df.rename(columns={"Valeur": "Daily change in cumulative total", "Indicateurs": "Date"})
+
+    df.loc[:, "Country"] = "France"
+    df.loc[:, "Units"] = "tests performed"
+    df.loc[:, "Testing type"] = "PCR only"
+    df.loc[:, "Cumulative total"] = pd.NA
+    df.loc[:, "Source URL"] = "https://geodes.santepubliquefrance.fr"
+    df.loc[:, "Source label"] = "National Public Health Agency"
+    df.loc[:, "Notes"] = pd.NA
+
+    df.to_csv("automated_sheets/France - tests performed.csv", index=False)
+
+
+def people_tested():
 
     url = "https://www.data.gouv.fr/fr/datasets/r/dd0de5d9-b5a5-4503-930a-7b08dc0adc7c"
     df = pd.read_csv(url, sep=";", usecols=["jour", "cl_age90", "T"])
@@ -17,13 +37,14 @@ def main():
     df.loc[:, "Country"] = "France"
     df.loc[:, "Units"] = "people tested"
     df.loc[:, "Testing type"] = "PCR only"
-    df.loc[:, "Cumulative total"] = np.nan
+    df.loc[:, "Cumulative total"] = pd.NA
     df.loc[:, "Source URL"] = "https://www.data.gouv.fr/fr/datasets/donnees-relatives-aux-resultats-des-tests-virologiques-covid-19/"
     df.loc[:, "Source label"] = "National Public Health Agency"
-    df.loc[:, "Notes"] = np.nan
+    df.loc[:, "Notes"] = pd.NA
 
     df.to_csv("automated_sheets/France - people tested.csv", index=False)
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    tests_performed()
+    people_tested()
