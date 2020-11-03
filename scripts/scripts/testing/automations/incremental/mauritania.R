7a6591fff37e2632c1e149336880ed210a17afc5
Edouard Mathieu <edmat@pm.me>
2020-11-03 12:34:03 +0100

    Better series for Mauritania

---
+++ scripts/scripts/testing/automations/incremental/mauritania.R
@@ -0,0 +1,46 @@
+url <- read_html("https://www.sante.gov.mr/?cat=4&paged=1") %>%
+    html_nodes(".post-title a") %>%
+    html_attr("href") %>%
+    sort()
+
+url <- url[url >= "https://www.sante.gov.mr/?p=5081"]
+
+process_article <- function(url) {
+    message(url)
+    url <- read_html(url) %>%
+        html_node(".post-content a") %>%
+        html_attr("href")
+    message(url)
+
+    if (is.na(url) | !str_detect(str_to_lower(url), "sitrep")) return(NULL)
+
+    download.file(url = url, destfile = "tmp/tmp.pdf", quiet = TRUE)
+
+    date <- NA
+    if (str_detect(url, "\\d\\d-\\d\\d-202\\d")) date <- str_extract(url, "\\d\\d-\\d\\d-202\\d") %>% dmy()
+    if (str_detect(url, "\\d{4}202\\d")) date <- str_extract(url, "\\d{4}202\\d") %>% dmy()
+    if (str_detect(url, "/2\\d{5}-")) date <- str_extract(url, "2\\d{5}") %>% ymd()
+    if (str_detect(url, "/\\d{4}2\\d-")) date <- str_extract(url, "/\\d{4}2\\d-") %>% dmy()
+    if (str_detect(url, "/2\\d{6}-")) date <- str_extract(url, "2\\d{6}") %>% str_sub(c(1, 6), c(4, 7)) %>% str_flatten() %>% ymd()
+    stopifnot(!is.na(date))
+
+    count <- pdf_text("tmp/tmp.pdf") %>%
+        str_extract("\\d+ ?tests ont été effectués") %>%
+        str_replace_all("[^\\d]", "") %>%
+        na.omit() %>%
+        as.integer() %>%
+        max()
+
+    add_snapshot(
+        count = count,
+        date = date,
+        sheet_name = "Mauritania",
+        country = "Mauritania",
+        units = "tests performed",
+        testing_type = "unclear",
+        source_url = url,
+        source_label = "Ministry of Health"
+    )
+}
+
+sapply(url, FUN=process_article)
