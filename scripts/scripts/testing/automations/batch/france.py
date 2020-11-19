1f97e957b19dc0cd59f0207f319f4f7b0a3383ef
Edouard Mathieu <edmat@pm.me>
2020-11-19 10:19:10 +0100

    Add antigen for France

--- scripts/scripts/testing/automations/batch/france.py
+++ scripts/scripts/testing/automations/batch/france.py
@@ -7,22 +7,32 @@ import numpy as np
 
 def tests_performed():
 
+    # PCR tests
     assert datetime.datetime.utcfromtimestamp(os.stat("input/data.xlsx").st_mtime).date() == datetime.date.today(), \
         "File is too old. Import new data from Geodes."
+    pcr = pd.read_excel("input/data.xlsx", sheet_name ="Graphiques", skiprows=3)
+    pcr = pcr.iloc[0:pcr[pcr.Indicateurs.isna()].index.min(), :]
+    pcr["Valeur"] = pcr["Valeur"].str.replace(r"\s", "").astype(int)
+    pcr = pcr.rename(columns={"Valeur": "PCR", "Indicateurs": "Date"})
 
-    df = pd.read_excel("input/data.xlsx", sheet_name ="Graphiques", skiprows=3)
-    df = df.iloc[0:df[df.Indicateurs.isna()].index.min(), :]
+    # Antigen tests
+    antigen = pd.read_csv("https://www.data.gouv.fr/fr/datasets/r/a4db9eea-9423-4fcd-8373-e2b1e3f27ad3", sep=";")
+    antigen = antigen[antigen["act_description"].isin(["FORFAIT COVID  AVEC PRÉLÈVEMENT", "FORFAIT COVID  SANS PRÉLÈVEMENT"])]
+    assert len(antigen) > 0
+    antigen = antigen[["date", "quantity"]].groupby("date", as_index=False).sum()
+    antigen = antigen.rename(columns={"quantity": "Antigen", "date": "Date"})
 
-    df["Valeur"] = df["Valeur"].str.replace(r"\s", "").astype(int)
-
-    df = df.rename(columns={"Valeur": "Daily change in cumulative total", "Indicateurs": "Date"})
+    df = pd.merge(pcr, antigen, on="Date", how="outer").fillna(0)
+    df.loc[:, "Daily change in cumulative total"] = df["PCR"].add(df["Antigen"]).astype(int)
+    import pdb; pdb.set_trace()
+    df = df.drop(columns=["PCR", "Antigen"])
 
     df.loc[:, "Country"] = "France"
     df.loc[:, "Units"] = "tests performed"
-    df.loc[:, "Testing type"] = "PCR only"
+    df.loc[:, "Testing type"] = "includes non-PCR"
     df.loc[:, "Cumulative total"] = pd.NA
-    df.loc[:, "Source URL"] = "https://geodes.santepubliquefrance.fr"
-    df.loc[:, "Source label"] = "National Public Health Agency"
+    df.loc[:, "Source URL"] = "https://geodes.santepubliquefrance.fr / https://www.data.gouv.fr/fr/datasets/covid-executions-et-ventes-de-tests-antigeniques-rapides-en-pharmacie/"
+    df.loc[:, "Source label"] = "National Public Health Agency / IQVIA France"
     df.loc[:, "Notes"] = pd.NA
 
     df.to_csv("automated_sheets/France - tests performed.csv", index=False)
