e5adf2e3ce8ae7471f03f2f48d9651f7549d7a83
Edouard Mathieu <edmat@pm.me>
2020-11-26 10:39:45 +0100

    Collect Lithuania in batch

---
+++ scripts/scripts/testing/automations/batch/lithuania.R
@@ -0,0 +1,18 @@
+df <- fread("https://opendata.arcgis.com/datasets/538b7bd574594daa86fefd16509cbc36_0.csv",
+            select = c("test_performed_date", "tests_total_cumulative"), showProgress = FALSE)
+
+setnames(df, "test_performed_date", "Date")
+
+df <- df[, .(`Cumulative total` = sum(tests_total_cumulative)), Date]
+df <- df[`Cumulative total` > 0]
+
+df[, Date := date(ymd_hms(Date))]
+
+df[, Country := "Lithuania"]
+df[, Units := "tests performed"]
+df[, `Testing type` := "PCR only"]
+df[, `Source label` := "Government of Lithuania"]
+df[, `Source URL` := "https://open-data-ls-osp-sdg.hub.arcgis.com/datasets/538b7bd574594daa86fefd16509cbc36_0"]
+df[, Notes := NA_character_]
+
+fwrite(df, "automated_sheets/Lithuania.csv")
