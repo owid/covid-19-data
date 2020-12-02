c39589194c0321aac8a06f5b1ed5d17f1af4364e
Edouard Mathieu <edmat@pm.me>
2020-12-02 18:10:56 +0100

    Use combined OxCGRT dataset
    BSG has now created a CSV that reports a single 'combined' value for each indicator where a local policy is treated as a half-step. For instance, school and university closures are recorded as C1=3 if they apply nationwide. If these closures only occured in one state or territory, not across the whole country, we would report C1_combined=2.5.

--- scripts/scripts/megafile.py
+++ scripts/scripts/megafile.py
@@ -220,7 +220,7 @@ def get_cgrt():
     """
 
     cgrt = pd.read_csv(
-        "https://raw.githubusercontent.com/OxCGRT/covid-policy-tracker/master/data/OxCGRT_latest.csv",
+        "https://raw.githubusercontent.com/OxCGRT/covid-policy-tracker/master/data/OxCGRT_latest_combined.csv",
         low_memory=False
     )
 
