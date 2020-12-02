39a92c53824e956b41d66b2012657ab0237d7451
Edouard Mathieu <edmat@pm.me>
2020-12-02 18:01:30 +0100

    Collect US positive rate from CDC

--- scripts/scripts/testing/automations/batch/united_states_hhs.R
+++ scripts/scripts/testing/automations/batch/united_states_hhs.R
@@ -1,3 +1,4 @@
+# Test counts
 meta <- fromJSON(file = "https://healthdata.gov/api/3/action/package_show?id=c13c00e3-f3d0-4d49-8c43-bf600a6c0a0d")
 url <- meta$result[[1]]$resources[[1]]$url
 
@@ -11,11 +12,28 @@ setorder(df, Date)
 # due to the time it takes to report testing information.
 df <- df[Date <= today() - 3]
 
+# Positive rate
+url <- read_html("https://www.cdc.gov/coronavirus/2019-ncov/covid-data/covidview/index.html") %>%
+    html_nodes(".syndicate a") %>%
+    html_attr("href") %>%
+    str_subset("csv$") %>%
+    paste0("https://www.cdc.gov", .)
+positive_rate <- fread(url, skip = 6, select = c("Week", "% of Specimens Positive for SARS-CoV-2"))
+positive_rate[, year := str_sub(Week, 1, 4)]
+positive_rate[, week_no := as.integer(str_sub(Week, 5, 6))]
+positive_rate[, Date := ymd(sprintf("%s0101", year))]
+week(positive_rate$Date) <- positive_rate$week_no
+positive_rate[, c("year", "week_no", "Week") := NULL]
+setnames(positive_rate, "% of Specimens Positive for SARS-CoV-2", "Positive rate")
+positive_rate[, `Positive rate` := round(`Positive rate` / 100, 3)]
+
+df <- merge(df, positive_rate, by = "Date", all.x = TRUE)
+
 df[, Country := "United States"]
 df[, Units := "tests performed"]
 df[, `Source label` := "Department of Health & Human Services"]
 df[, `Source URL` := url]
-df[, `Testing type` := "unclear"]
+df[, `Testing type` := "PCR"]
 df[, Notes := NA_character_]
 
 fwrite(df, "automated_sheets/United States - tests performed.csv")
