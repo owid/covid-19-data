e4396ce6434bfedf1451a5715c408232c2bc8d3b
Edouard Mathieu <edmat@pm.me>
2020-12-16 09:09:50 +0100

    Update collection for Mexico
    Remove cutoff and collect PR from source

--- scripts/scripts/testing/automations/batch/mexico.R
+++ scripts/scripts/testing/automations/batch/mexico.R
@@ -3,24 +3,29 @@ formatted_date <- format.Date(today() - 1, "%Y%m%d")
 confirmados <- sprintf("https://datos.covid-19.conacyt.mx/Downloads/Files/Casos_Diarios_Estado_Nacional_Confirmados_%s.csv", formatted_date)
 negativos <- sprintf("https://datos.covid-19.conacyt.mx/Downloads/Files/Casos_Diarios_Estado_Nacional_Negativos_%s.csv", formatted_date)
 
-process_file <- function(url) {
+process_file <- function(url, metric) {
     df <- fread(url, showProgress = FALSE)
     df <- df[nombre == "Nacional"] %>%
         gather(Date, Count, 4:ncol(df)) %>%
         data.table()
     df[, c("cve_ent", "poblacion", "nombre") := NULL]
     df[, Date := dmy(Date)]
+    setnames(df, "count", metric)
     return(df)
 }
 
-confirmados <- process_file(confirmados)
-negativos <- process_file(negativos)
+confirmados <- process_file(confirmados, "positive")
+negativos <- process_file(negativos, "negative")
 
 df <- merge(confirmados, negativos, by = "Date", all = TRUE)
-df[is.na(Count.x), Count.x := 0]
-df[is.na(Count.y), Count.y := 0]
-df[, `Daily change in cumulative total` := Count.x + Count.y]
-df[, c("Count.x", "Count.y") := NULL]
+df[is.na(positive), positive := 0]
+df[is.na(negative), negative := 0]
+df[, `Daily change in cumulative total` := positive + negative]
+
+setorder(df, Date)
+df[, `Positive rate` := round(frollsum(positive, 7) / frollsum(`Daily change in cumulative total`, 7), 3)]
+
+df[, c("positive", "negative") := NULL]
 df <- df[`Daily change in cumulative total` != 0]
 
 df[, Country := "Mexico"]
