ccecf980b6305fce6cd7217c5dbf32e2bdf7fc7a
Edouard Mathieu <edmat@pm.me>
2020-12-18 15:21:04 +0100

    Update czechia.R

--- scripts/scripts/testing/automations/batch/czechia.R
+++ scripts/scripts/testing/automations/batch/czechia.R
@@ -1,7 +1,7 @@
-df <- fread("https://onemocneni-aktualne.mzcr.cz/api/v1/covid-19/testy.csv",
-            select = c("datum", "testy_celkem"), showProgress = FALSE)
+df <- fread("https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/testy.csv",
+            select = c("datum", "kumulativni_pocet_testu"), showProgress = FALSE)
 
-setnames(df, c("datum", "testy_celkem"), c("Date", "Cumulative total"))
+setnames(df, c("datum", "kumulativni_pocet_testu"), c("Date", "Cumulative total"))
 
 df <- df[, .(Date = min(Date)), `Cumulative total`]
 
@@ -10,6 +10,5 @@ df[, Units := "tests performed"]
 df[, `Source URL` := "https://onemocneni-aktualne.mzcr.cz/api/v1/covid-19/testy.csv"]
 df[, `Source label` := "Ministry of Health"]
 df[, Notes := NA_character_]
-df[, `Testing type` := "PCR only"]
 
 fwrite(df, "automated_sheets/Czechia.csv")
