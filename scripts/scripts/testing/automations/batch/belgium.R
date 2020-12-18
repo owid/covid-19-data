11604ade871d04e2473f78bdf14261c33cfaf663
Edouard Mathieu <edmat@pm.me>
2020-12-18 15:05:28 +0100

    Collect PR for Belgium

--- scripts/scripts/testing/automations/batch/belgium.R
+++ scripts/scripts/testing/automations/batch/belgium.R
@@ -1,15 +1,17 @@
 df <- fread("https://epistat.sciensano.be/Data/COVID19BE_tests.csv", showProgress = FALSE)
-setnames(df, c("DATE", "TESTS_ALL"), c("Date", "Daily change in cumulative total"))
 
-df <- df[, .(`Daily change in cumulative total` = sum(`Daily change in cumulative total`)), Date]
+df <- df[, .(TESTS_ALL = sum(TESTS_ALL), TESTS_ALL_POS = sum(TESTS_ALL_POS)), DATE]
+
+setorder(df, DATE)
+df[, PR := round(frollsum(TESTS_ALL_POS, 7) / frollsum(TESTS_ALL, 7), 3)]
+df[, TESTS_ALL_POS := NULL]
+
+setnames(df, c("DATE", "TESTS_ALL", "PR"), c("Date", "Daily change in cumulative total", "Positive rate"))
 
 df[, Country := "Belgium"]
 df[, Units := "tests performed"]
 df[, `Source URL` := "https://epistat.sciensano.be/Data/COVID19BE_tests.csv"]
 df[, `Source label` := "Sciensano (Belgian institute for health)"]
 df[, Notes := NA_character_]
-df[, `Testing type` := "unclear"]
-
-df <- df[ymd(Date) <= today()]
 
 fwrite(df, "automated_sheets/Belgium.csv")
