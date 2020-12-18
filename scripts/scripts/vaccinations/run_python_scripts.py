bc02b3a2aa4fb576ba476f4181b445f085c1a2a8
Edouard Mathieu <edmat@pm.me>
2020-12-18 19:03:13 +0100

    Add automation runner

---
+++ scripts/scripts/vaccinations/run_python_scripts.py
@@ -0,0 +1,21 @@
+import sys
+import os
+import re
+from glob import glob
+import datetime
+
+SKIP = []
+
+execution_mode = sys.argv[1]
+scripts_path = "automations/incremental/*.py" if execution_mode == "quick" else "automations/*/*.py"
+
+scripts = glob(scripts_path)
+
+if len(SKIP) > 0:
+    print(f"Warning message:\nSkipping the following countries: {', '.join(SKIP)}")
+    SKIP = "|".join(SKIP)
+    scripts = [s for s in scripts if not bool(re.search(pattern=SKIP, string=s))]
+
+for script_name in scripts:
+    print(f"{datetime.datetime.now().replace(microsecond=0)} - {script_name}")
+    os.system(f"python3 {script_name}")
