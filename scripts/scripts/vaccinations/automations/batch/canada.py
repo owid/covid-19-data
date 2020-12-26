d04d460e96922feeee97bd726369e88cc2adb03a
Edouard Mathieu <edmat@pm.me>
2020-12-26 13:44:48 +0100

    Data update

--- scripts/scripts/vaccinations/automations/batch/canada.py
+++ scripts/scripts/vaccinations/automations/batch/canada.py
@@ -13,6 +13,8 @@ def main():
     })
 
     df["date"] = pd.to_datetime(df["date"], format="%d-%m-%Y")
+
+    df = df.groupby("total_vaccinations", as_index=False).min()
     
     df.loc[:, "location"] = "Canada"
     df.loc[:, "vaccine"] = "Pfizer/BioNTech"
