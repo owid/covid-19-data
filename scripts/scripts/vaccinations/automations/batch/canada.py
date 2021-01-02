35e595f6e71f311bcbbdd60e128d359b9972dcd6
Edouard Mathieu <edmat@pm.me>
2021-01-02 23:07:25 +0100

    Vaccination update

--- scripts/scripts/vaccinations/automations/batch/canada.py
+++ scripts/scripts/vaccinations/automations/batch/canada.py
@@ -17,7 +17,8 @@ def main():
     df = df.groupby("total_vaccinations", as_index=False).min()
     
     df.loc[:, "location"] = "Canada"
-    df.loc[:, "vaccine"] = "Moderna, Pfizer/BioNTech"
+    df.loc[:, "vaccine"] = "Pfizer/BioNTech"
+    df.loc[df["date"] >= "2020-12-31", "vaccine"] = "Moderna, Pfizer/BioNTech"
     df.loc[:, "source_url"] = "https://github.com/ishaberry/Covid19Canada/blob/master/timeseries_canada/vaccine_administration_timeseries_canada.csv"
 
     df.to_csv("automations/output/Canada.csv", index=False)
