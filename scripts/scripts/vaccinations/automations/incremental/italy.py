66a496248ede149b664060222552471591a7ea7c
Edouard Mathieu <edmat@pm.me>
2021-01-05 10:54:05 +0100

    Collect date

--- scripts/scripts/vaccinations/automations/incremental/italy.py
+++ scripts/scripts/vaccinations/automations/incremental/italy.py
@@ -12,30 +12,36 @@ def main():
 
     op = Options()
     op.add_argument("--headless")
-    driver = webdriver.Chrome(
-        executable_path="automations\\incremental\\chromedriver.exe",
-        options=op
-    )
-    url = "https://app.powerbi.com/view?r=eyJrIjoiMzg4YmI5NDQtZDM5ZC00ZTIyLTgxN2MtOTBkMWM4MTUyYTg0IiwidCI6ImFmZDBhNzVjLTg2NzEtNGNjZS05MDYxLTJjYTBkOTJlNDIyZiIsImMiOjh9"
-    driver.get(url)
-    # Wait for  the desired element to load. If nothing is found
-    # after 25 seconds, returns.
-    timeout = 25
-    try:
-        element_present = EC.presence_of_element_located((By.CLASS_NAME, "value"))
-        data = WebDriverWait(driver, timeout).until(element_present).text
-    except TimeoutException:
-        return
-    count = int(data.replace(",", ""))
+    
+    with webdriver.Chrome(options=op) as driver:
 
-    vaxutils.increment(
-        location="Italy",
-        total_vaccinations=count,
-        date=str(datetime.date.today()),
-        source_url=url,
-        vaccine="Pfizer/BioNTech"
-    )
-    driver.close()
+        url = "https://app.powerbi.com/view?r=eyJrIjoiMzg4YmI5NDQtZDM5ZC00ZTIyLTgxN2MtOTBkMWM4MTUyYTg0IiwidCI6ImFmZDBhNzVjLTg2NzEtNGNjZS05MDYxLTJjYTBkOTJlNDIyZiIsImMiOjh9"
+        driver.get(url)
+
+        # Wait for the desired element to load. If nothing is found after 25 seconds, returns.
+        timeout = 25
+
+        try:
+            element_present = EC.presence_of_element_located((By.CLASS_NAME, "value"))
+            data = WebDriverWait(driver, timeout).until(element_present).text
+        except TimeoutException:
+            return
+        count = vaxutils.clean_count(data)
+
+        try:
+            element_present = EC.presence_of_element_located((By.CLASS_NAME, "title"))
+            data = WebDriverWait(driver, timeout).until(element_present).text
+        except TimeoutException:
+            return
+        date = vaxutils.clean_date(data, "%m/%d/%Y %H:%M:%S %p")
+
+        vaxutils.increment(
+            location="Italy",
+            total_vaccinations=count,
+            date=date,
+            source_url=url,
+            vaccine="Pfizer/BioNTech"
+        )
 
 
 if __name__ == "__main__":
