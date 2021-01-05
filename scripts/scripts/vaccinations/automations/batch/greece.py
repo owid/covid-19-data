0806fd158b463d66878ec10fed8aaa709284bb1f
Edouard Mathieu <edmat@pm.me>
2021-01-05 21:44:03 +0100

    Automate vax for Greece

---
+++ scripts/scripts/vaccinations/automations/batch/greece.py
@@ -0,0 +1,23 @@
+import json
+import requests
+import pandas as pd
+
+url = "https://data.gov.gr/api/v1/query/mdg_emvolio?date_from=2020-12-29&date_to=2021-01-05"
+
+with open("vax_dataset_config.json", "rb") as file:
+    config = json.loads(file.read())
+
+response = requests.get(url, headers={"Authorization": f"Token {config['greece_api_token']}"})
+
+df = pd.DataFrame.from_records(response.json())
+
+df = df.groupby("referencedate", as_index=False)[["referencedate", "totalvaccinations"]].sum()
+
+df = df.rename(columns={"referencedate": "date", "totalvaccinations": "total_vaccinations"})
+
+df.loc[:, "date"] = df["date"].str.slice(0, 10)
+df.loc[:, "location"] = "Greece"
+df.loc[:, "vaccine"] = "Pfizer/BioNTech"
+df.loc[:, "source_url"] = "https://www.data.gov.gr/datasets/mdg_emvolio/"
+
+df.to_csv("automations/output/Greece.csv", index=False)
