900b8acf034a5537815c8d9338414ec62d22dc5b
Edouard Mathieu <edmat@pm.me>
2021-01-07 16:07:58 +0100

    Fix Slovenia importer

--- scripts/scripts/vaccinations/automations/batch/slovenia.py
+++ scripts/scripts/vaccinations/automations/batch/slovenia.py
@@ -3,11 +3,11 @@ import pandas as pd
 def main():
 
     df = pd.read_csv(
-        "https://raw.githubusercontent.com/sledilnik/data/master/csv/stats-weekly.csv",
-        usecols=["date", "week.vaccination.administered"]
+        "https://raw.githubusercontent.com/sledilnik/data/master/csv/vaccination.csv",
+        usecols=["date", "vaccination.administered.todate"]
     )
 
-    df = df.rename(columns={"week.vaccination.administered": "total_vaccinations"})
+    df = df.rename(columns={"vaccination.administered.todate": "total_vaccinations"})
     df = df[-df["total_vaccinations"].isna()]
 
     df.loc[:, "location"] = "Slovenia"
