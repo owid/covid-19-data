d6ffc9dcacf18615983744ca3b86d22aef00ed54
Edouard Mathieu <edmat@pm.me>
2021-01-12 21:18:38 +0100

    Automate vax collection for Israel

---
+++ scripts/scripts/vaccinations/automations/batch/israel.py
@@ -0,0 +1,34 @@
+import json
+import requests
+import pandas as pd
+
+
+def main():
+
+    url = "https://datadashboardapi.health.gov.il/api/queries/vaccinated"
+
+    data = json.loads(requests.get(url).content)
+
+    df = pd.DataFrame.from_records(data)
+
+    df = df.rename(columns={
+        "Day_Date": "date",
+        "vaccinated_cum": "people_vaccinated",
+        "vaccinated_seconde_dose_cum": "people_fully_vaccinated"
+    })
+
+    df["total_vaccinations"] = df["people_vaccinated"].add(df["people_fully_vaccinated"])
+
+    df["date"] = df["date"].str.slice(0, 10)
+
+    df = df[["date", "total_vaccinations", "people_vaccinated", "people_fully_vaccinated"]]
+
+    df.loc[:, "location"] = "Israel"
+    df.loc[:, "source_url"] = "https://datadashboard.health.gov.il/COVID-19/general"
+    df.loc[:, "vaccine"] = "Pfizer/BioNTech"
+
+    df.to_csv("automations/output/Israel.csv", index=False)
+
+
+if __name__ == "__main__":
+    main()
