47a7eeac0dd18f3d4053e798143394f7a8377135
Edouard Mathieu <edmat@pm.me>
2021-01-12 11:19:14 +0100

    Vaccination update

--- scripts/scripts/vaccinations/automations/batch/united_kingdom.py
+++ scripts/scripts/vaccinations/automations/batch/united_kingdom.py
@@ -1,15 +1,38 @@
 import pandas as pd
 
+def get_metrics(metrics, area):
+    metrics = "".join([f"&metric={m}" for m in metrics])
+    url = f"https://api.coronavirus.data.gov.uk/v2/data?areaType={area}&format=csv{metrics}"
+    df = pd.read_csv(url)
+    return df
+
 def main():
 
-    uk = pd.read_csv("https://api.coronavirus.data.gov.uk/v2/data?areaType=overview&metric=cumPeopleReceivingFirstDose&metric=cumPeopleReceivingSecondDose&format=csv")
-    subnational = pd.read_csv("https://api.coronavirus.data.gov.uk/v2/data?areaType=nation&metric=cumPeopleReceivingFirstDose&metric=cumPeopleReceivingSecondDose&format=csv")
+    assert False
 
-    df = pd.concat([uk, subnational])
+    metrics = [
+        "cumPeopleVaccinatedFirstDoseByPublishDate",
+        "cumPeopleVaccinatedFirstDoseByVaccinationDate",
+        "cumPeopleVaccinatedSecondDoseByPublishDate",
+        "cumPeopleVaccinatedSecondDoseByVaccinationDate",
+    ]
 
+    uk = get_metrics(metrics, "overview")
+    subnational = get_metrics(metrics, "nation")
+
+    # df = pd.concat([uk, subnational])
+    df = uk.copy()
     df = df.rename(columns={"areaName": "location"})
 
-    df["total_vaccinations"] = df["cumPeopleReceivingFirstDose"] + df["cumPeopleReceivingSecondDose"]
+    df["dose1"] = df.cumPeopleVaccinatedFirstDoseByPublishDate
+    df.loc[df["dose1"].isna(), "dose1"] = df.cumPeopleVaccinatedFirstDoseByVaccinationDate
+
+    df["dose2"] = df.cumPeopleVaccinatedSecondDoseByPublishDate
+    df.loc[df["dose2"].isna(), "dose2"] = df.cumPeopleVaccinatedSecondDoseByVaccinationDate
+
+    import pdb; pdb.set_trace()
+
+    df["total_vaccinations"] = df["dose1"] + df["dose2"]
 
     df["people_fully_vaccinated"] = df["cumPeopleReceivingSecondDose"].replace({0: pd.NA})
 
