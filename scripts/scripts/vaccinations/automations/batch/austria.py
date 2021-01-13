f681075ace22839649327467228f4c0c93983924
Edouard Mathieu <edmat@pm.me>
2021-01-13 18:27:21 +0100

    Update to new column name

--- scripts/scripts/vaccinations/automations/batch/austria.py
+++ scripts/scripts/vaccinations/automations/batch/austria.py
@@ -7,8 +7,8 @@ def main():
 
     url = "https://info.gesundheitsministerium.gv.at/data/national.csv"
 
-    df = pd.read_csv(url, sep=";", usecols=["Datum", "geimpft"])
-    df = df.rename(columns={"Datum": "date", "geimpft": "total_vaccinations"})
+    df = pd.read_csv(url, sep=";", usecols=["Datum", "Impfungen"])
+    df = df.rename(columns={"Datum": "date", "Impfungen": "total_vaccinations"})
 
     df = df[(df["total_vaccinations"].notnull()) & (df["total_vaccinations"] > 0)]
     df = df.groupby("total_vaccinations", as_index=False).min()
