322da957ba14a4b222b703898ad5fc839225df7a
Edouard Mathieu <edmat@pm.me>
2021-01-15 00:09:17 +0100

    Vaccination update

--- scripts/scripts/vaccinations/automations/batch/netherlands.py
+++ scripts/scripts/vaccinations/automations/batch/netherlands.py
@@ -4,6 +4,7 @@ def main():
 
     df = pd.read_csv("https://github.com/YorickBleijenberg/COVID_data_RIVM_Netherlands/raw/master/vaccination/people.vaccinated.csv")
     df.loc[:, "location"] = "Netherlands"
+    df = df.sort_values("date").groupby("total_vaccinations", as_index=False).head(1)
     df.to_csv("automations/output/Netherlands.csv", index=False)
 
 if __name__ == '__main__':
