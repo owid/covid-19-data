4e0db63ca6d7ede4ccf63c3253a0a2d02bb5f4e8
Edouard Mathieu <edmat@pm.me>
2021-01-14 12:55:01 +0100

    Automate vax collection for Turkey

---
+++ scripts/scripts/vaccinations/automations/incremental/turkey.py
@@ -0,0 +1,27 @@
+import datetime
+import json
+import pytz
+import requests
+import vaxutils
+
+
+def main():
+
+    url = "https://covid19asi.saglik.gov.tr/covid19asisayisi?getir=asiYapilanKisiSayisi"
+    data = json.loads(requests.get(url).content)
+
+    count = data["asisayisi"]
+
+    date = str(datetime.datetime.now(pytz.timezone("Asia/Istanbul")).date())
+
+    vaxutils.increment(
+        location="Turkey",
+        total_vaccinations=count,
+        date=date,
+        source_url="https://covid19asi.saglik.gov.tr/",
+        vaccine="Sinopharm"
+    )
+
+
+if __name__ == '__main__':
+    main()
