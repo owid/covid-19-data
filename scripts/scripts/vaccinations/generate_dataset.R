48112c2e58a78c762d11fedff4a16d4804c5f87b
Edouard Mathieu <edmat@pm.me>
2021-01-14 23:30:47 +0100

    Check that total_vaccinations values are unique

--- scripts/scripts/vaccinations/generate_dataset.R
+++ scripts/scripts/vaccinations/generate_dataset.R
@@ -101,6 +101,7 @@ process_location <- function(location_name) {
 
     # Sanity checks
     stopifnot(length(unique(df$date)) == nrow(df))
+    stopifnot(length(unique(df$total_vaccinations)) == nrow(df))
 
     if (!"people_vaccinated" %in% names(df)) {
         df[, people_vaccinated := total_vaccinations]
