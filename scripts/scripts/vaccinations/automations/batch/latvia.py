7f33ce7c815300591fc05d2d24f5933faac41d8f
Edouard Mathieu <edmat@pm.me>
2021-01-14 23:25:01 +0100

    Update vax importer for Latvia

--- scripts/scripts/vaccinations/automations/batch/latvia.py
+++ scripts/scripts/vaccinations/automations/batch/latvia.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+
 def main():
 
     url = "https://data.gov.lv/dati/eng/dataset/covid19-vakcinacijas"
@@ -9,22 +10,26 @@ def main():
 
     file_url = soup.find_all("a", class_="resource-url-analytics")[-1]["href"]
     
-    df = pd.read_excel(file_url, usecols=["Vakcinācijas datums", "Vakcinēto personu skaits"])
+    df = pd.read_excel(file_url, skiprows=2)
 
-    df = df.rename(columns={
-        "Vakcinācijas datums": "date",
-        "Vakcinēto personu skaits": "total_vaccinations"
-    })
+    df = df[df["Vakcinācijas iestādes kods"] == "Kopā"]
 
-    df = df.groupby("date", as_index=False).sum()
+    df = df.drop(columns=["Vakcinācijas iestādes kods", "Vakcinācijas iestādes nosaukums", "Kopā"])
+
+    df = df.melt(var_name="date", value_name="daily_vaccinations")
+
     df = df.sort_values("date")
-    df["total_vaccinations"] = df["total_vaccinations"].cumsum()
+
+    df["total_vaccinations"] = df["daily_vaccinations"].cumsum().astype(int)
+
+    df = df.drop(columns=["daily_vaccinations"])
 
     df.loc[:, "location"] = "Latvia"
     df.loc[:, "vaccine"] = "Pfizer/BioNTech"
     df.loc[:, "source_url"] = url
 
     df.to_csv("automations/output/Latvia.csv", index=False)
+
 
 if __name__ == '__main__':
     main()
