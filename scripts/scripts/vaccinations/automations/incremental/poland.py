620b823290b04c6da65ba1d61114bcf22ab0ee65
Edouard Mathieu <edmat@pm.me>
2021-01-15 16:00:40 +0100

    Automate vax collection for Poland

---
+++ scripts/scripts/vaccinations/automations/incremental/poland.py
@@ -0,0 +1,29 @@
+import datetime
+import json
+import pytz
+import requests
+import pandas as pd
+import vaxutils
+
+
+def main():
+
+    url = "https://services9.arcgis.com/RykcEgwHWuMsJXPj/arcgis/rest/services/global_szczepienia_widok2/FeatureServer/0/query?f=json&where=Data%20BETWEEN%20(CURRENT_TIMESTAMP%20-%20INTERVAL%20%2724%27%20HOUR)%20AND%20CURRENT_TIMESTAMP&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&resultOffset=0&resultRecordCount=1&resultType=standard"
+    data = json.loads(requests.get(url).content)
+
+    total_vaccinations = data["features"][0]["attributes"]["SZCZEPIENIA_SUMA"]
+
+    date = data["features"][0]["attributes"]["Data"]
+    date = str((pd.to_datetime(date, unit="ms").date() - pd.DateOffset(days=1)).date())
+
+    vaxutils.increment(
+        location="Poland",
+        total_vaccinations=total_vaccinations,
+        date=date,
+        source_url="https://www.gov.pl/web/szczepimysie/raport-szczepien-przeciwko-covid-19",
+        vaccine="Pfizer/BioNTech"
+    )
+
+
+if __name__ == "__main__":
+    main()
