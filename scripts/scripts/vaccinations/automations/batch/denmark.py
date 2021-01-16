8be84619af1b0f327e384706919b96dcecca81ea
Maximilian Scheid <m@scheid.dk>
2021-01-16 17:33:40 +0100

    Update denmark.py
    Add info about total vaccinations and fully vaccinated people

--- scripts/scripts/vaccinations/automations/batch/denmark.py
+++ scripts/scripts/vaccinations/automations/batch/denmark.py
@@ -14,15 +14,22 @@ def main():
 
     # Fetch data
     df = pd.DataFrame()
-    column_string = {'dtype': str}  # Force dtype to be object because of thousand separator in Europe
-    kwargs = {'pandas_options': column_string}
+    column_string = {'dtype': str , 'header': None}  # Force dtype to be object because of thousand separator in Europe
+    kwargs = {'pandas_options': column_string,}
     dfs_from_pdf = tabula.read_pdf(pdf_path, pages="all", **kwargs)
-    vaccinated_df = dfs_from_pdf[0]  # Hardcoded table location
+    vaccinated_df = pd.DataFrame(dfs_from_pdf[0])  # Hardcoded table location
+    header = vaccinated_df[0:3] # concat rows to header
+    header = header.astype(str).apply(lambda x: x.replace("nan", "")).apply(' '.join).apply(lambda x: x.strip())
+    vaccinated_df.columns = header
+    vaccinated_df = vaccinated_df.drop([0,1,2])
+    
 
     # Manipulate data
     df["date"] = pd.to_datetime(vaccinated_df["Vaccinationsdato"], format="%d-%m-%Y")
     vaccinated_df.columns = vaccinated_df.columns.str.replace(r"\s", " ", regex=True)
-    df["total_vaccinations"] = vaccinated_df["Antal personer som har påbegyndt covid-19 vaccination"].apply(lambda x: x.replace(".", "")).astype(int)
+    df["people_vaccinated"] = vaccinated_df["Antal personer som har påbegyndt covid-19- vaccination"].apply(lambda x: x.replace(".", "")).astype(int)
+    df["people_fully_vaccinated"] = vaccinated_df["Antal personer som er færdigvaccineret pr. dag"].apply(lambda x: x.replace(".", "").replace("-", "0")).astype(int)
+    df["total_vaccinations"] = df["people_vaccinated"] + df["people_fully_vaccinated"]
     df = df.groupby("total_vaccinations", as_index=False).min()
     
     df.loc[:, "location"] = "Denmark"
