e74fad62a2d1dead6d8ecdcb38bd884a24f7c794
Edouard Mathieu <edmat@pm.me>
2021-01-22 15:27:25 +0100

    Collect 2nd dose data for Belgium

--- scripts/scripts/vaccinations/automations/batch/belgium.py
+++ scripts/scripts/vaccinations/automations/batch/belgium.py
@@ -3,18 +3,21 @@ import pandas as pd
 
 def main():
 
-    url = "https://github.com/bmesuere/covid/raw/master/data/vaccines_cumulative.csv"
-    df = pd.read_csv(url)
+    url = "https://epistat.sciensano.be/Data/COVID19BE_VACC.csv"
+    df = pd.read_csv(url, usecols=["Date", "PartlyVaccinated", "FullyVaccinated"])
 
-    df = df[df["REGION"] == "Belgium"].rename(columns={
-        "DATE": "date", "REGION": "location", "VACCINES_TOTAL": "total_vaccinations"
+    df = df.rename(columns={
+        "Date": "date",
+        "PartlyVaccinated": "people_vaccinated",
+        "FullyVaccinated": "people_fully_vaccinated"
     })
 
-    df = df.groupby(["location", "total_vaccinations"], as_index=False).min()
+    df = df.groupby("date", as_index=False).sum().sort_values("date")
+    df["people_vaccinated"] = df["people_vaccinated"].cumsum()
+    df["people_fully_vaccinated"] = df["people_fully_vaccinated"].cumsum()
+    df["total_vaccinations"] = df["people_vaccinated"] + df["people_fully_vaccinated"]
 
-    df["people_vaccinated"] = df["total_vaccinations"]
-    df["people_fully_vaccinated"] = pd.NA
-
+    df.loc[:, "location"] = "Belgium"
     df.loc[:, "vaccine"] = "Pfizer/BioNTech"
     df.loc[:, "source_url"] = "https://datastudio.google.com/embed/u/0/reporting/c14a5cfc-cab7-4812-848c-0369173148ab/page/hOMwB"
 
