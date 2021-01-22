d4a098f1048efe32263233958b1400f3934d6a02
Edouard Mathieu <edmat@pm.me>
2021-01-22 17:34:01 +0100

    Update data correction for Austria

--- scripts/scripts/testing/testing_data_corrections.R
+++ scripts/scripts/testing/testing_data_corrections.R
@@ -1,3 +1,3 @@
-# January 2021: large negative drop in cumulative tests in Austria, leading to very high PR for one week
-collated[Country == "Austria" & Date >= "2021-01-02" & Date <= "2021-01-09", `Short-term positive rate` := NA]
-collated[Country == "Austria" & Date >= "2021-01-02" & Date <= "2021-01-09", `Short-term tests per case` := NA]
+# January 2021: very large changes in cumulative tests in Austria, leading to unrealistic PRs
+collated[Country == "Austria" & Date >= "2021-01-02", `Short-term positive rate` := NA]
+collated[Country == "Austria" & Date >= "2021-01-02", `Short-term tests per case` := NA]
