c6e461637b8e82fae926c53e2d014feb15308614
Edouard Mathieu <edmat@pm.me>
2021-01-23 20:10:01 +0100

    Vaccination update

--- scripts/scripts/vaccinations/automations/batch/slovenia.py
+++ scripts/scripts/vaccinations/automations/batch/slovenia.py
@@ -4,11 +4,16 @@ def main():
 
     df = pd.read_csv(
         "https://raw.githubusercontent.com/sledilnik/data/master/csv/vaccination.csv",
-        usecols=["date", "vaccination.administered.todate"]
+        usecols=["date", "vaccination.administered.todate", "vaccination.administered2nd.todate"]
     )
 
-    df = df.rename(columns={"vaccination.administered.todate": "total_vaccinations"})
-    df = df[-df["total_vaccinations"].isna()]
+    df = df.rename(columns={
+        "vaccination.administered.todate": "people_vaccinated",
+        "vaccination.administered2nd.todate": "people_fully_vaccinated"
+    })
+
+    df = df[-df["people_vaccinated"].isna()]
+    df["total_vaccinations"] = df["people_vaccinated"] + df["people_fully_vaccinated"].fillna(0)
 
     df.loc[:, "location"] = "Slovenia"
     df.loc[:, "source_url"] = "https://covid-19.sledilnik.org/en/stats"
