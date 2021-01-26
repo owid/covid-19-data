e416239ac6cce6989a1d7f90b9910f0e16eacc26
Ondrej Kokes <ondrej.kokes@gmail.com>
2021-01-26 08:41:25 +0100

    New vaccination data for Czechia

--- scripts/scripts/vaccinations/automations/batch/czechia.py
+++ scripts/scripts/vaccinations/automations/batch/czechia.py
@@ -1,33 +1,41 @@
-import os
-import requests
-from bs4 import BeautifulSoup
 import pandas as pd
 
 
 def main():
      
-    url = "https://onemocneni-aktualne.mzcr.cz/vakcinace-cr"
-    soup = BeautifulSoup(requests.get(url).content, "html.parser")
-    data_link = soup.find("a", text="Data ke stažení").attrs["href"]
+    source_url = "https://onemocneni-aktualne.mzcr.cz/covid-19"
+    data_url = "https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/ockovani.csv"
 
-    df = pd.read_csv(data_link, parse_dates=["datum_vakcinace"])
-    assert df.columns.tolist() == ["datum_vakcinace", "vykázaná očkování"]
+    df = pd.read_csv(data_url, parse_dates=["datum"])
+    assert df.columns.tolist() == ["datum", "vakcina", "kraj_nuts_kod", "kraj_nazev", "vekova_skupina",
+       "prvnich_davek", "druhych_davek", "celkem_davek"]
+
+    # since we need to translate vaccine names, we'll check that no new
+    # manufacturers were added, so that we can maintain control over this
+    assert set(df["vakcina"].unique()) == {"Comirnaty", "COVID-19 Vaccine Moderna"}
+    df = df.replace({
+        "Comirnaty": "Pfizer/BioNTech",
+        "COVID-19 Vaccine Moderna": "Moderna",
+    })
+
+    df = df.groupby("datum").agg(
+        vaccine=("vakcina", lambda x: ", ".join(sorted(list(set(x))))),
+        total_vaccinations=("celkem_davek", "sum"),
+        people_vaccinated=("prvnich_davek", "sum"),
+        people_fully_vaccinated=("druhych_davek", "sum"),
+    ).reset_index()
+
     df = df.rename(columns={
-        "datum_vakcinace": "date",
-        "vykázaná očkování": "total_vaccinations",
+         "datum": "date",
     })
     df["date"] = df["date"].astype(str).str.slice(0, 10)
     df = df.sort_values("date")
 
-    assert (df["total_vaccinations"] > 0).all()
-    df["total_vaccinations"] = df["total_vaccinations"].cumsum()
+    for col in ["total_vaccinations", "people_vaccinated", "people_fully_vaccinated"]:
+        df[col] = df[col].cumsum()
 
     df.loc[:, "location"] = "Czechia"
-    df.loc[:, "source_url"] = url
-
-    df.loc[:, "vaccine"] = "Pfizer/BioNTech"
-    df.loc[df.date >= "2021-01-17", "vaccine"] = "Moderna, Pfizer/BioNTech"
-
+    df.loc[:, "source_url"] = source_url
     df.to_csv("automations/output/Czechia.csv", index=False)
 
 
