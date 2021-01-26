3e1db6653a10d43ff3b6e6555a4dd869e127a2c0
Edouard Mathieu <edmat@pm.me>
2021-01-26 16:21:31 +0100

    Collect vax data for Slovakia from IZA + dashboard

--- scripts/scripts/vaccinations/automations/batch/slovakia.py
+++ scripts/scripts/vaccinations/automations/batch/slovakia.py
@@ -1,31 +1,64 @@
+import json
+import requests
 import pandas as pd
 
 
-def main():
+def import_iza():
 
-    df = pd.read_csv(
+    # The online dashboard only starts on January 20, 2021.
+    # Previous data is collected from the repo maintained by IZA:
+    # https://github.com/Institut-Zdravotnych-Analyz/covid19-data
+    iza = pd.read_csv(
         "https://raw.githubusercontent.com/Institut-Zdravotnych-Analyz/covid19-data/main/OpenData_Slovakia_Vaccination_Regions.csv",
         usecols=["Date", "first_dose", "second_dose"],
         sep=";"
     )
 
-    df = (
-        df
-        .groupby("Date", as_index=False)
+    iza = (
+        iza.groupby("Date", as_index=False)
         .sum()
         .rename(columns={
-            "Date": "date", "first_dose": "people_vaccinated", "second_dose": "people_fully_vaccinated"
+            "Date": "date",
+            "first_dose": "people_vaccinated",
+            "second_dose": "people_fully_vaccinated"
         })
         .sort_values("date")
     )
 
-    df["people_vaccinated"] = df["people_vaccinated"].cumsum()
-    df["people_fully_vaccinated"] = df["people_fully_vaccinated"].cumsum()
+    iza["people_vaccinated"] = iza["people_vaccinated"].cumsum()
+    iza["people_fully_vaccinated"] = iza["people_fully_vaccinated"].cumsum()
+    iza["total_vaccinations"] = iza["people_vaccinated"] + iza["people_fully_vaccinated"]
+    iza["people_fully_vaccinated"] = iza["people_fully_vaccinated"].replace(0, pd.NA)
+    iza["source_url"] = "https://github.com/Institut-Zdravotnych-Analyz/covid19-data/blob/main/OpenData_Slovakia_Vaccination_Regions.csv"
+
+    return iza
+
+
+def import_dashboard():
+
+    url = "https://covid-19.nczisk.sk/webapi/v1/kpi?e916b653e44d1fdf57b5bbe826b624b7"
+    data = json.loads(requests.get(url).content)
+
+    df = pd.DataFrame.from_records(data["tiles"]["k31"]["data"]["d"])
+    df = df[["d", "v", "v1"]].rename(columns={
+        "d": "date", "v": "people_vaccinated", "v1": "people_fully_vaccinated"
+    })
+    df["date"] = pd.to_datetime(df["date"], format="%y%m%d").dt.date.astype(str)
     df["total_vaccinations"] = df["people_vaccinated"] + df["people_fully_vaccinated"]
-    df["people_fully_vaccinated"] = df["people_fully_vaccinated"].replace(0, pd.NA)
+    df["source_url"] = "https://covid-19.nczisk.sk"
+
+    return df
+
+
+def main():
+
+    iza = import_iza()
+    dashboard = import_dashboard()
+
+    iza = iza[iza["date"] < dashboard["date"].min()]
+    df = pd.concat([iza, dashboard]).sort_values("date")
 
     df.loc[:, "location"] = "Slovakia"
-    df.loc[:, "source_url"] = "https://github.com/Institut-Zdravotnych-Analyz/covid19-data/blob/main/OpenData_Slovakia_Vaccination_Regions.csv"
     df.loc[:, "vaccine"] = "Pfizer/BioNTech"
 
     df.to_csv("automations/output/Slovakia.csv", index=False)
