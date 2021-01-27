4ca928dd422b9f7d54bf38e12efe54ef22848cb1
Edouard Mathieu <edmat@pm.me>
2021-01-27 15:53:04 +0100

    Add manual data point for 2nd doses in Portugal

--- scripts/scripts/vaccinations/automations/incremental/portugal.py
+++ scripts/scripts/vaccinations/automations/incremental/portugal.py
@@ -10,13 +10,13 @@ def main():
     url = "https://services5.arcgis.com/eoFbezv6KiXqcnKq/arcgis/rest/services/Covid19_Total_Vacinados/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&resultOffset=0&resultRecordCount=50&resultType=standard&cacheHint=true"
     data = json.loads(requests.get(url).content)
 
-    count = data["features"][0]["attributes"]["Vacinados_Ac"]
+    total_vaccinations = data["features"][0]["attributes"]["Vacinados_Ac"]
 
     date = str(datetime.datetime.now(pytz.timezone("Europe/Lisbon")).date())
 
     vaxutils.increment(
         location="Portugal",
-        total_vaccinations=count,
+        total_vaccinations=total_vaccinations,
         date=date,
         source_url="https://covid19.min-saude.pt/ponto-de-situacao-atual-em-portugal/",
         vaccine="Pfizer/BioNTech"
