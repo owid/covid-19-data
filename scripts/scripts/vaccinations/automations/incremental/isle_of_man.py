4634e637e0b3c8fb577f13ac26b0f397ddf8634b
Edouard Mathieu <edmat@pm.me>
2021-01-27 17:06:04 +0100

    Fix Isle of Man scraper

--- scripts/scripts/vaccinations/automations/incremental/isle_of_man.py
+++ scripts/scripts/vaccinations/automations/incremental/isle_of_man.py
@@ -9,16 +9,26 @@ def main():
     url = "https://covid19.gov.im/general-information/latest-updates/"
     soup = BeautifulSoup(requests.get(url).content, "html.parser")
 
-    date = soup.find(class_="fa-syringe").parent.find("strong").text
+    vax_box = soup.find(string=re.compile("Total first vaccinations")).parent.parent
+
+    date = vax_box.find("strong").text
     date = vaxutils.clean_date(date, "%d %B %Y")
 
-    total_vaccinations = soup.find(class_="fa-syringe").parent.text
-    total_vaccinations = re.search(r"Total vaccinations:\s+(\d+)", total_vaccinations).group(1)
-    total_vaccinations = vaxutils.clean_count(total_vaccinations)
+    for p in vax_box.find_all("p"):
+        if "Total first vaccinations" in p.text:
+            data = p.text
+
+    people_vaccinated, people_fully_vaccinated = re.search(r"Total first vaccinations:\xa0 ([\d,]+)Total second vaccinations:\xa0 ([\d,]+)", data).groups()
+
+    people_vaccinated = vaxutils.clean_count(people_vaccinated)
+    people_fully_vaccinated = vaxutils.clean_count(people_fully_vaccinated)
+    total_vaccinations = people_vaccinated + people_fully_vaccinated
 
     vaxutils.increment(
         location="Isle of Man",
         total_vaccinations=total_vaccinations,
+        people_vaccinated=people_vaccinated,
+        people_fully_vaccinated=people_fully_vaccinated,
         date=date,
         source_url=url,
         vaccine="Pfizer/BioNTech"
