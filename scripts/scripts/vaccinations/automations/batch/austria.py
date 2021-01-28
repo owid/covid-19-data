5dede04b9b2dac827503e5906598514563c8e396
Edouard Mathieu <edmat@pm.me>
2021-01-28 14:55:02 +0100

    Collect Austrian vax from new official dataset

--- scripts/scripts/vaccinations/automations/batch/austria.py
+++ scripts/scripts/vaccinations/automations/batch/austria.py
@@ -4,21 +4,25 @@ import pandas as pd
 
 def main():
 
-    url = "https://info.gesundheitsministerium.gv.at/data/national.csv"
+    url = "https://info.gesundheitsministerium.gv.at/data/timeline.csv"
 
     df = pd.read_csv(url, sep=";")
 
-    assert df.shape[1] == 3
+    assert df.shape[1] == 14
 
-    df = df[["Datum", "Auslieferungen"]]
+    df = df[df["Name"] == "Österreich"]
+    df = df[["Datum", "EingetrageneImpfungen", "Teilgeimpfte", "Vollimmunisierte"]]
 
-    df = df.rename(columns={"Datum": "date", "Auslieferungen": "total_vaccinations"})
+    df = df.rename(columns={
+        "Datum": "date",
+        "EingetrageneImpfungen": "total_vaccinations",
+        "Teilgeimpfte": "people_vaccinated",
+        "Vollimmunisierte": "people_fully_vaccinated"
+    })
 
-    df = df[(df["total_vaccinations"].notnull()) & (df["total_vaccinations"] > 0) & (df["date"] <= str(datetime.date.today()))]
-    df = df.groupby("total_vaccinations", as_index=False).min()
-
+    df.loc[:, "date"] = df.date.str.slice(0, 10)
     df.loc[:, "location"] = "Austria"
-    df.loc[:, "source_url"] = "https://info.gesundheitsministerium.gv.at/opendata.html"
+    df.loc[:, "source_url"] = "https://www.data.gv.at/katalog/dataset/589132b2-c000-4c60-85b4-c5036cdf3406"
     df.loc[:, "vaccine"] = "Pfizer/BioNTech"
 
     df.to_csv("automations/output/Austria.csv", index=False)
