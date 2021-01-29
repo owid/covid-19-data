a6b05d305c3f5d99795e303b83829c75e0aa8223
Edouard Mathieu <edmat@pm.me>
2021-01-29 18:26:32 +0100

    Archive vax script for Belgium

--- scripts/scripts/vaccinations/automations/archived/belgium.py
+++ scripts/scripts/vaccinations/automations/archived/belgium.py
@@ -1,23 +1,27 @@
-import os
 import pandas as pd
 
 
 def main():
 
-    os.system("curl -s -O https://covid-vaccinatie.be/api/vaccines-administered.json")
+    url = "https://github.com/bmesuere/covid/raw/master/data/VACC.csv"
+    df = pd.read_csv(url, usecols=["Date", "PartlyVaccinated", "FullyVaccinated"])
 
-    df = pd.read_json("vaccines-administered.json")
+    df = df.rename(columns={
+        "Date": "date",
+        "PartlyVaccinated": "people_vaccinated",
+        "FullyVaccinated": "people_fully_vaccinated"
+    })
 
-    df = df.sort_values(["date", "cumulative"]).groupby("date", as_index=False).tail(1)
-    df = df.drop(columns=["amount"]).rename(columns={"cumulative": "total_vaccinations"})
+    df = df.groupby("date", as_index=False).sum().sort_values("date")
+    df["people_vaccinated"] = df["people_vaccinated"].cumsum()
+    df["people_fully_vaccinated"] = df["people_fully_vaccinated"].cumsum()
+    df["total_vaccinations"] = df["people_vaccinated"] + df["people_fully_vaccinated"]
 
     df.loc[:, "location"] = "Belgium"
     df.loc[:, "vaccine"] = "Pfizer/BioNTech"
-    df.loc[:, "source_url"] = "https://covid-vaccinatie.be/en"
+    df.loc[:, "source_url"] = "https://datastudio.google.com/embed/u/0/reporting/c14a5cfc-cab7-4812-848c-0369173148ab/page/hOMwB"
 
     df.to_csv("automations/output/Belgium.csv", index=False)
-
-    os.remove("vaccines-administered.json")
 
 
 if __name__ == '__main__':
