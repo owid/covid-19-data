824c720b6cdcf0baf2ca09f0762e9960f6253bbf
Edouard Mathieu <edmat@pm.me>
2021-01-29 18:27:07 +0100

    New file format

--- scripts/scripts/vaccinations/automations/batch/france.py
+++ scripts/scripts/vaccinations/automations/batch/france.py
@@ -5,13 +5,12 @@ def main():
 
     url = "https://www.data.gouv.fr/fr/datasets/r/efe23314-67c4-45d3-89a2-3faef82fae90"
     df = pd.read_csv(url, sep=";")
-    assert df.shape[1] == 3
+    assert df.shape[1] == 4
 
-    df = df.sort_values("jour").drop(columns="fra").rename(columns={
-        "n_dose1": "people_vaccinated", "jour": "date"
+    df = df.sort_values("jour").drop(columns=["fra", "n_dose1"]).rename(columns={
+        "n_cum_dose1": "people_vaccinated", "jour": "date"
     })
 
-    df.loc[:, "people_vaccinated"] = df["people_vaccinated"].cumsum()
     df.loc[:, "total_vaccinations"] = df["people_vaccinated"]
 
     df.loc[:, "location"] = "France"
