649fea86bd919ff5aec9a567bd11a8f9e4984830
Edouard Mathieu <edmat@pm.me>
2021-02-05 09:40:31 +0100

    Collect US vax in batch with new CDC dtaa

---
+++ scripts/scripts/vaccinations/automations/batch/united_states.py
@@ -0,0 +1,35 @@
+import requests
+import pandas as pd
+import vaxutils
+
+
+def main():
+    
+    url = "https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=vaccination_trends_data"
+    data = requests.get(url).json()["vaccination_trends_data"]
+
+    df = pd.DataFrame.from_records(data)
+    df = df[df["ShortName"] == "USA"]
+
+    df = df[["LongName", "Date", "Administered_Cumulative", "Admin_Dose_1_Cumulative", "Admin_Dose_2_Cumulative"]]
+    df = df.rename(columns={
+        "LongName": "location",
+        "Administered_Cumulative": "total_vaccinations",
+        "Admin_Dose_1_Cumulative": "people_vaccinated",
+        "Admin_Dose_2_Cumulative": "people_fully_vaccinatede",
+        "Date": "date"  
+    })
+
+    # Remove rows with unchanged data
+    df = df.dropna().sort_values("date").groupby("total_vaccinations").head(1)
+
+    df.loc[:, "source_url"] = "https://covid.cdc.gov/covid-data-tracker/#vaccination-trends"
+
+    df.loc[:, "vaccine"] = "Pfizer/BioNTech"
+    df.loc[df["date"] >= "2020-12-23", "vaccine"] = "Moderna, Pfizer/BioNTech"
+
+    df.to_csv("automations/output/United States.csv", index=False)
+
+
+if __name__ == "__main__":
+    main()
