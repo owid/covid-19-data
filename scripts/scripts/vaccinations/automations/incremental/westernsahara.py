b30e96efa1ed18c1a0818576e1989d5f43ad22e1
covid19owid <covid19owid@gmail.com>
2021-02-06 14:19:57 -0500

    Automated Western Sahara  vaccination data pull
     Automated Western Sahara  vaccination data pull
    
    Source:
    https://covid.hespress.com/fr/

---
+++ scripts/scripts/vaccinations/automations/incremental/westernsahara.py
@@ -0,0 +1,42 @@
+import datetime
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import vaxutils
+
+
+def main():
+
+    # Options for Chrome WebDriver
+    op = Options()
+    op.add_argument("--headless")
+
+    with webdriver.Chrome(options=op) as driver:
+
+        url = "https://covid.hespress.com/fr/"
+        driver.get(url)
+        time.sleep(5)
+
+        for elem in driver.find_elements_by_class_name("card-title"):
+            if "Bénéficiaires de la vaccination" in elem.text:
+                cnt = elem.find_element_by_xpath("//h5/following-sibling::h4")
+                people_vaccinated = vaxutils.clean_count(cnt.text)
+                total_vaccinations = vaxutils.clean_count(cnt.text)
+
+    people_vaccinated = people_vaccinated
+    total_vaccinations = total_vaccinations
+
+    date = datetime.date.today() - datetime.timedelta(days=1)
+
+    vaxutils.increment(
+        location="Western Sahara",
+        total_vaccinations=total_vaccinations,
+        people_vaccinated=people_vaccinated,
+        date=date,
+        source_url=url,
+        vaccine="Oxford/AstraZeneca, Sinopharm"
+    )
+
+
+if __name__ == "__main__":
+    main()
