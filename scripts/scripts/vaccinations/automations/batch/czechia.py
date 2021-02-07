b410b5748aff4d086175a10c6148a8248e2fa2c1
Ondrej Kokes <ondrej.kokes@gmail.com>
2021-02-07 09:59:19 +0100

    vaccination in czechia: new datasource
    we now get data on every single vaccination (so the dataset currently
    has 350k rows), the aggregate dataset will get discontinued in a few
    days

--- scripts/scripts/vaccinations/automations/batch/czechia.py
+++ scripts/scripts/vaccinations/automations/batch/czechia.py
@@ -9,30 +9,30 @@ vaccine_mapping = {
 def main():
 
     source_url = "https://onemocneni-aktualne.mzcr.cz/covid-19"
-    data_url = "https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/ockovani.csv"
+    data_url = "https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/ockovaci-mista.csv"
 
     df = pd.read_csv(data_url, parse_dates=["datum"])
-    assert df.columns.tolist() == ["datum", "vakcina", "kraj_nuts_kod", "kraj_nazev", "vekova_skupina",
-       "prvnich_davek", "druhych_davek", "celkem_davek"]
+    assert df.columns.tolist() == ["datum", "vakcina", "kraj_nuts_kod", "kraj_nazev", "zarizeni_kod",
+       "zarizeni_nazev", "poradi_davky", "vekova_skupina"]
 
     # since we need to translate vaccine names, we'll check that no new
     # manufacturers were added, so that we can maintain control over this
     # IMPORTANT: If a new vaccine is added, see if it requires a single dose
     # or two doses. If it's a single-dose one, make sure to fix the calculation
-    # of `people_fully_vaccinated` and an assertion about basic arithmetics
+    # of `total_vaccinations`
     assert set(df["vakcina"].unique()) == set(vaccine_mapping.keys())
     df = df.replace(vaccine_mapping)
 
+    df = df.groupby(["datum", "vakcina", "poradi_davky"]).size().unstack().reset_index()
     df = df.groupby("datum").agg(
         vaccine=("vakcina", lambda x: ", ".join(sorted(set(x)))),
-        total_vaccinations=("celkem_davek", "sum"),
-        people_vaccinated=("prvnich_davek", "sum"),
-        # the following holds only because all vaccines used so far require two doses
-        people_fully_vaccinated=("druhych_davek", "sum"),
+        people_vaccinated=(1, "sum"),  # 1 means 1st dose
+        people_fully_vaccinated=(2, "sum"),
     ).reset_index()
 
-    # this holds as long as all vaccines require two doses
-    assert (df["total_vaccinations"] == df["people_vaccinated"] + df["people_fully_vaccinated"]).all()
+    # the following holds only because all vaccines used so far require two doses
+    df["total_vaccinations"] = df["people_vaccinated"] + df["people_fully_vaccinated"]
+
 
     df = df.rename(columns={
          "datum": "date",
@@ -42,7 +42,7 @@ def main():
     assert df["date"].min() == "2020-12-27"
 
     for col in ["total_vaccinations", "people_vaccinated", "people_fully_vaccinated"]:
-        df[col] = df[col].cumsum()
+        df[col] = df[col].cumsum().astype(int)
 
     df.loc[:, "location"] = "Czechia"
     df.loc[:, "source_url"] = source_url
