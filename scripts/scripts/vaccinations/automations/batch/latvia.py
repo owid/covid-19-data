79834c8305f92d593802c6f26f02958a1f10d961
Edouard Mathieu <edmat@pm.me>
2021-02-09 17:28:35 +0100

    Latvia/vax: collect data by vaccine

--- scripts/scripts/vaccinations/automations/batch/latvia.py
+++ scripts/scripts/vaccinations/automations/batch/latvia.py
@@ -11,19 +11,38 @@ def main():
     file_url = soup.find_all("a", class_="resource-url-analytics")[-1]["href"]
     
     df = pd.read_excel(file_url, usecols=[
-        "Vakcinācijas datums", "Vakcinēto personu skaits", "Vakcinācijas posms"
+        "Vakcinācijas datums", "Vakcinēto personu skaits", "Vakcinācijas posms", "Preparāts"
     ])
+
+    vaccine_mapping = {
+        "Comirnaty": "Pfizer/BioNTech",
+        "COVID-19 Vaccine Moderna": "Moderna",
+    }
+    df["Preparāts"] = df["Preparāts"].str.strip()
+    assert set(df["Preparāts"].unique()) == set(vaccine_mapping.keys())
+    df = df.replace(vaccine_mapping)
+
+    # Data by vaccine
+    vax = (
+        df.groupby(["Vakcinācijas datums", "Preparāts"], as_index=False)
+        ["Vakcinēto personu skaits"].sum()
+        .sort_values("Vakcinācijas datums")
+        .rename(columns={"Vakcinācijas datums": "date", "Preparāts": "vaccine", "Vakcinēto personu skaits": "total_vaccinations"})
+    )
+    vax["total_vaccinations"] = vax.groupby("vaccine", as_index=False)["total_vaccinations"].cumsum()
+    vax["location"] = "Latvia"
+    vax.to_csv("automations/output/by_vaccine/Latvia.csv", index=False)
 
     df = df.rename(columns={
         "Vakcinācijas datums": "date",
-        "Vakcinēto personu skaits": "total_vaccinations",
+        "Vakcinēto personu skaits": "quantity",
         "Vakcinācijas posms": "dose_number"
     })
 
     df = (
         df.groupby(["date", "dose_number"], as_index=False)
         .sum()
-        .pivot(index="date", columns="dose_number", values="total_vaccinations")
+        .pivot(index="date", columns="dose_number", values="quantity")
         .reset_index()
         .sort_values("date")
     )
