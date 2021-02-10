d64cedf9a46aadef94d478e40b3a181f49ed7d35
Edouard Mathieu <edmat@pm.me>
2021-02-10 11:12:06 +0100

    Austria/vax: use new regional file for total_vaccinations

--- scripts/scripts/vaccinations/automations/batch/austria.py
+++ scripts/scripts/vaccinations/automations/batch/austria.py
@@ -5,7 +5,7 @@ def read(source: str) -> pd.DataFrame:
     return pd.read_csv(source, sep=";")
 
 
-def check_columns(input: pd.DataFrame, expected=14) -> pd.DataFrame:
+def check_columns(input: pd.DataFrame, expected) -> pd.DataFrame:
     n_columns = input.shape[1]
     if n_columns != expected:
         raise ValueError(
@@ -20,19 +20,12 @@ def filter_country(input: pd.DataFrame) -> pd.DataFrame:
     return input[input["Name"] == "Österreich"]
 
 
-def select_columns(input: pd.DataFrame) -> pd.DataFrame:
-    return input[["Datum", "EingetrageneImpfungen", "Teilgeimpfte", "Vollimmunisierte"]]
+def select_columns(input: pd.DataFrame, columns: list) -> pd.DataFrame:
+    return input[columns]
 
 
-def rename_columns(input: pd.DataFrame) -> pd.DataFrame:
-    return input.rename(
-        columns={
-            "Datum": "date",
-            "EingetrageneImpfungen": "total_vaccinations",
-            "Teilgeimpfte": "people_vaccinated",
-            "Vollimmunisierte": "people_fully_vaccinated",
-        }
-    )
+def rename_columns(input: pd.DataFrame, columns: dict) -> pd.DataFrame:
+    return input.rename(columns=columns)
 
 
 def format_date(input: pd.DataFrame) -> pd.DataFrame:
@@ -42,27 +35,54 @@ def format_date(input: pd.DataFrame) -> pd.DataFrame:
 def enrich_columns(input: pd.DataFrame) -> pd.DataFrame:
     return input.assign(
         location="Austria",
-        source_url="https://www.data.gv.at/katalog/dataset/589132b2-c000-4c60-85b4-c5036cdf3406",
+        source_url="https://info.gesundheitsministerium.gv.at/opendata/",
         vaccine="Moderna, Oxford/AstraZeneca, Pfizer/BioNTech",
     )
 
 
-def pipeline(input: pd.DataFrame) -> pd.DataFrame:
+def pipeline_vaccinations(input: pd.DataFrame) -> pd.DataFrame:
+    return (
+        input.pipe(check_columns, expected=6)
+        .pipe(filter_country)
+        .pipe(select_columns, columns=["Datum", "GemeldeteImpfungenLaender"])
+        .pipe(rename_columns, columns={
+            "Datum": "date",
+            "GemeldeteImpfungenLaender": "total_vaccinations",
+        })
+        .pipe(format_date)
+        .dropna()
+    )
+
+
+def pipeline_people(input: pd.DataFrame) -> pd.DataFrame:
     return (
         input.pipe(check_columns, expected=14)
         .pipe(filter_country)
-        .pipe(select_columns)
-        .pipe(rename_columns)
+        .pipe(select_columns, columns=["Datum", "Teilgeimpfte", "Vollimmunisierte"])
+        .pipe(rename_columns, columns={
+            "Datum": "date",
+            "Teilgeimpfte": "people_vaccinated",
+            "Vollimmunisierte": "people_fully_vaccinated",
+        })
         .pipe(format_date)
-        .pipe(enrich_columns)
+        .dropna()
     )
 
 
 def main():
-    source = "https://info.gesundheitsministerium.gv.at/data/timeline.csv"
+    source_vaccinations = "https://info.gesundheitsministerium.gv.at/data/timeline-bundeslaendermeldungen.csv"
+    source_people = "https://info.gesundheitsministerium.gv.at/data/timeline.csv"
     destination = "automations/output/Austria.csv"
 
-    read(source).pipe(pipeline).to_csv(destination, index=False)
+    vaccinations = read(source_vaccinations).pipe(pipeline_vaccinations)
+    people = read(source_people).pipe(pipeline_people)
+
+    (
+        pd.merge(vaccinations, people, on="date", how="outer")
+        .pipe(enrich_columns)
+        .sort_values("date")
+        .to_csv(destination, index=False)
+    )
 
 
 if __name__ == "__main__":
