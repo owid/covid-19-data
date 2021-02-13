b0a6ad5f154ca0974e48f10bffa8ebbddd0819aa
covid19owid <covid19owid@gmail.com>
2021-02-13 00:52:59 -0500

    Update incremental pipelines with Series

--- scripts/scripts/vaccinations/automations/incremental/liechtenstein.py
+++ scripts/scripts/vaccinations/automations/incremental/liechtenstein.py
@@ -4,57 +4,48 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import vaxutils
 
-date = None
 
-
-def read(source: str) -> pd.DataFrame:
+def read(source: str) -> pd.Series:
     soup = BeautifulSoup(requests.get(source).content, "html.parser")
     table = soup.find(class_="geo-unit-vacc-doses-data__table")
-    global date
     date = soup.find(class_="detail-card__source").find("span").text
     date = re.search(r"[\d\.]{10}", date).group(0)
     date = str(date)
     date = vaxutils.clean_date(date, "%d.%m.%Y")
-    return pd.read_html(str(table))[0]
+    total_vaccinations = pd.read_html(str(table))[0].loc[1]['absolute numbers']
+    return pd.Series([total_vaccinations], index=['total_vaccinations']).append(pd.Series([date], index=['date']))
 
 
-def add_totals(input: pd.DataFrame) -> pd.DataFrame:
-    return input.assign(
-        total_vaccinations=
-        input.loc[input["per 100 inhabitants"] == "Administered vaccines", "absolute numbers"].values[0],
-    )
+def enrich_location(input: pd.Series) -> pd.Series:
+    return vaxutils.enrich_data(input, 'location', "Liechtenstein")
 
 
-def format_date(input: pd.DataFrame) -> pd.DataFrame:
-    return input.assign(date=date)
+def enrich_vaccine(input: pd.Series) -> pd.Series:
+    return vaxutils.enrich_data(input, 'vaccine', "Moderna, Pfizer/BioNTech")
 
 
-def enrich_columns(input: pd.DataFrame) -> pd.DataFrame:
-    return input.assign(
-        location="Liechtenstein",
-        vaccine="Moderna, Pfizer/BioNTech",
-        source_url="https://www.covid19.admin.ch/en/epidemiologic/vacc-doses?detGeo=FL",
-    )
+def enrich_source(input: pd.Series) -> pd.Series:
+    return vaxutils.enrich_data(input, 'source_url',
+                                "https://www.covid19.admin.ch/en/epidemiologic/vacc-doses?detGeo=FL")
 
 
-def pipeline(input: pd.DataFrame) -> pd.DataFrame:
+def pipeline(input: pd.Series) -> pd.Series:
     return (
-        input.pipe(add_totals)
-            .pipe(format_date)
-            .pipe(enrich_columns)
+        input.pipe(enrich_location)
+            .pipe(enrich_vaccine)
+            .pipe(enrich_source)
     )
 
 
 def main():
     source = "https://www.covid19.admin.ch/en/epidemiologic/vacc-doses?detGeo=FL"
     data = read(source).pipe(pipeline)
-
     vaxutils.increment(
-        location=str(data['location'].values[0]),
-        total_vaccinations=int(data['total_vaccinations'].values[0]),
-        date=str(data['date'].values[0]),
-        source_url=str(data['source_url'].values[0]),
-        vaccine=str(data['vaccine'].values[0])
+        location=str(data['location']),
+        total_vaccinations=int(data['total_vaccinations']),
+        date=str(data['date']),
+        source_url=str(data['source_url']),
+        vaccine=str(data['vaccine'])
     )
 
 
