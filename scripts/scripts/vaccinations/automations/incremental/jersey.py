c9c50dfbd3c63bb6a50f3da14d2ecc638e056237
covid19owid <covid19owid@gmail.com>
2021-02-13 00:52:22 -0500

    Update incremental pipelines with Series

--- scripts/scripts/vaccinations/automations/incremental/jersey.py
+++ scripts/scripts/vaccinations/automations/incremental/jersey.py
@@ -4,45 +4,47 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import vaxutils
 
-date = None
 
-
-def read(source: str) -> pd.DataFrame:
+def read(source: str) -> pd.Series:
     soup = BeautifulSoup(requests.get(source).content, "html.parser")
     table = soup.find(class_="govstyleTable-default")
-    global date
-    date = re.search(r"Data applies to: Week ending (\d[\w\s]+\d{4})", soup.text).group(1)
-    date = str(date)
-    date = vaxutils.clean_date(date, "%d %B %Y")
-    return pd.read_html(str(table))[0]
+    df = pd.read_html(str(table))[0]
+    df.set_index(df.columns[0], inplace=True)
+    ds = df.T.squeeze()
+    dt = re.search(r"Data applies to: Week ending (\d[\w\s]+\d{4})", soup.text).group(1)
+    dt = str(dt)
+    dt = vaxutils.clean_date(dt, "%d %B %Y")
+    return ds.append(pd.Series([dt], index=['date']))
 
 
-def add_totals(input: pd.DataFrame) -> pd.DataFrame:
-    return input.assign(
-        people_vaccinated=int(input.loc[input[0] == "Total number of doses", 1].values[0]),
-        people_fully_vaccinated=int(input.loc[input[0] == "Total number of first dose vaccinations", 1].values[0]),
-    ).assign(
-        total_vaccinations=int(input.loc[input[0] == "Total number of second dose vaccinations", 1].values[0]),
-    )
+def enrich_columns(input: pd.Series) -> pd.Series:
+    input.rename(
+        index={'Total number of first dose vaccinations': 'people_vaccinated',
+               'Total number of second dose vaccinations': 'people_fully_vaccinated',
+               'Total number of doses': 'total_vaccinations'},
+        inplace=True)
+    return input
 
 
-def format_date(input: pd.DataFrame) -> pd.DataFrame:
-    return input.assign(date=date)
+def enrich_location(input: pd.Series) -> pd.Series:
+    return vaxutils.enrich_data(input, 'location', "Jersey")
 
 
-def enrich_columns(input: pd.DataFrame) -> pd.DataFrame:
-    return input.assign(
-        location="Jersey",
-        vaccine="Oxford/AstraZeneca, Pfizer/BioNTech",
-        source_url="https://www.gov.je/Health/Coronavirus/Vaccine/Pages/VaccinationStatistics.aspx",
-    )
+def enrich_vaccine(input: pd.Series) -> pd.Series:
+    return vaxutils.enrich_data(input, 'vaccine', "Oxford/AstraZeneca, Pfizer/BioNTech")
 
 
-def pipeline(input: pd.DataFrame) -> pd.DataFrame:
+def enrich_source(input: pd.Series) -> pd.Series:
+    return vaxutils.enrich_data(input, 'source_url',
+                                "https://www.gov.je/Health/Coronavirus/Vaccine/Pages/VaccinationStatistics.aspx")
+
+
+def pipeline(input: pd.Series) -> pd.Series:
     return (
-        input.pipe(add_totals)
-            .pipe(format_date)
-            .pipe(enrich_columns)
+        input.pipe(enrich_columns)
+            .pipe(enrich_location)
+            .pipe(enrich_vaccine)
+            .pipe(enrich_source)
     )
 
 
@@ -51,13 +53,13 @@ def main():
     data = read(source).pipe(pipeline)
 
     vaxutils.increment(
-        location=str(data['location'].values[0]),
-        total_vaccinations=int(data['total_vaccinations'].values[0]),
-        people_vaccinated=int(data['people_vaccinated'].values[0]),
-        people_fully_vaccinated=int(data['people_fully_vaccinated'].values[0]),
-        date=str(data['date'].values[0]),
-        source_url=str(data['source_url'].values[0]),
-        vaccine=str(data['vaccine'].values[0])
+        location=str(data['location']),
+        total_vaccinations=int(data['total_vaccinations']),
+        people_vaccinated=int(data['people_vaccinated']),
+        people_fully_vaccinated=int(data['people_fully_vaccinated']),
+        date=str(data['date']),
+        source_url=str(data['source_url']),
+        vaccine=str(data['vaccine'])
     )
 
 
