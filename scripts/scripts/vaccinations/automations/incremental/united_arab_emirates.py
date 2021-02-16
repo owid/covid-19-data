ca44f0af26790fd311b33b5c024b21552b90d506
covid19owid <covid19owid@gmail.com>
2021-02-15 23:16:54 -0500

    Refactor the pipeline for UAE

--- scripts/scripts/vaccinations/automations/incremental/united_arab_emirates.py
+++ scripts/scripts/vaccinations/automations/incremental/united_arab_emirates.py
@@ -1,26 +1,52 @@
-import pytz
-import datetime
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 import vaxutils
+import datetime
+import pytz
 
-def main():
-    url = "http://covid19.ncema.gov.ae/en"
-    soup = BeautifulSoup(requests.get(url).content, "html.parser")
 
     total_vaccinations = soup.find(class_="doses").find(class_="counter").text
-    total_vaccinations = vaxutils.clean_count(total_vaccinations)
+    return vaxutils.clean_count(total_vaccinations)
 
+
+def format_date(input: pd.Series) -> pd.Series:
     date = str(datetime.datetime.now(pytz.timezone("Asia/Dubai")).date() - datetime.timedelta(days=1))
+    return vaxutils.enrich_data(input, 'date', date)
 
+
+def enrich_location(input: pd.Series) -> pd.Series:
+    return vaxutils.enrich_data(input, 'location', "United Arab Emirates")
+
+
+def enrich_vaccine(input: pd.Series) -> pd.Series:
+    return vaxutils.enrich_data(input, 'vaccine',
+                                "Oxford/AstraZeneca, Pfizer/BioNTech, Sinopharm/Beijing, Sinopharm/Wuhan, Sputnik V")
+
+
+def enrich_source(input: pd.Series) -> pd.Series:
+    return vaxutils.enrich_data(input, 'source_url',
+                                "http://covid19.ncema.gov.ae/en")
+
+
+def pipeline(input: pd.Series) -> pd.Series:
+    return (
+        input.pipe(format_date)
+            .pipe(enrich_location)
+            .pipe(enrich_vaccine)
+            .pipe(enrich_source)
+    )
+
+
+def main():
+    source = "http://covid19.ncema.gov.ae/en"
+    data = read(source).pipe(pipeline)
     vaxutils.increment(
-        location="United Arab Emirates",
-        total_vaccinations=total_vaccinations,
-        # people_vaccinated=people_vaccinated,
-        # people_fully_vaccinated=people_fully_vaccinated,
-        date=date,
-        source_url=url,
-        vaccine="Oxford/AstraZeneca, Pfizer/BioNTech, Sinopharm/Beijing, Sinopharm/Wuhan, Sputnik V"
+        location=str(data['location']),
+        total_vaccinations=int(data['total_vaccinations']),
+        date=str(data['date']),
+        source_url=str(data['source_url']),
+        vaccine=str(data['vaccine'])
     )
 
 
