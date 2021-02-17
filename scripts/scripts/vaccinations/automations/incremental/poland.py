4d5b8af1e2c8c18d9d51a60c501439bb63644502
covid19owid <covid19owid@gmail.com>
2021-02-17 18:41:22 -0500

    Update the pipeline for Poland

--- scripts/scripts/vaccinations/automations/incremental/poland.py
+++ scripts/scripts/vaccinations/automations/incremental/poland.py
@@ -9,30 +9,29 @@ def read(source: str) -> pd.Series:
     return parse_data(data)
 
 
-def parse_data(data: pd.DataFrame) -> pd.Series:
-    data = {'date': parse_date(data), 'total_vaccinations': parse_total_vaccinations(data),
-            'people_fully_vaccinated': parse_people_fully_vaccinated(data)}
+def parse_data(data: dict) -> pd.Series:
+    keys = ("date", "total_vaccinations", "people_fully_vaccinated")
+    values = (parse_date(data), parse_total_vaccinations(data), parse_people_fully_vaccinated(data))
+    data = dict(zip(keys, values))
     return pd.Series(data=data)
 
 
-def parse_date(data: pd.DataFrame) -> str:
+def parse_date(data: dict) -> str:
     date = data["Data"]
     date = str((pd.to_datetime(date, unit="ms").date() - pd.DateOffset(days=1)).date())
     return date
 
 
-def parse_total_vaccinations(data: pd.DataFrame) -> str:
-    total_vaccinations = data["SZCZEPIENIA_SUMA"]
-    return total_vaccinations
+def parse_total_vaccinations(data: dict) -> int:
+    return int(data["SZCZEPIENIA_SUMA"])
 
 
-def parse_people_fully_vaccinated(data: pd.DataFrame) -> str:
-    people_fully_vaccinated = data["DAWKA_2_SUMA"]
-    return people_fully_vaccinated
+def parse_people_fully_vaccinated(data: dict) -> int:
+    return int(data["DAWKA_2_SUMA"])
 
 
 def add_totals(input: pd.Series) -> pd.Series:
-    people_vaccinated = int(input['total_vaccinations']) - int(input['people_fully_vaccinated'])
+    people_vaccinated = input['total_vaccinations'] - input['people_fully_vaccinated']
     return vaxutils.enrich_data(input, 'people_vaccinated', people_vaccinated)
 
 
@@ -61,13 +60,13 @@ def main():
     source = "https://services-eu1.arcgis.com/zk7YlClTgerl62BY/arcgis/rest/services/global_szczepienia_widok3/FeatureServer/0/query?f=json&where=Data%20BETWEEN%20(CURRENT_TIMESTAMP%20-%20INTERVAL%20%2724%27%20HOUR)%20AND%20CURRENT_TIMESTAMP&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&resultOffset=0&resultRecordCount=1&resultType=standard"
     data = read(source).pipe(pipeline)
     vaxutils.increment(
-        location=str(data['location']),
-        total_vaccinations=int(data['total_vaccinations']),
-        people_vaccinated=int(data['people_vaccinated']),
-        people_fully_vaccinated=int(data['people_fully_vaccinated']),
-        date=str(data['date']),
-        source_url=str(data['source_url']),
-        vaccine=str(data['vaccine'])
+        location=data['location'],
+        total_vaccinations=data['total_vaccinations'],
+        people_vaccinated=data['people_vaccinated'],
+        people_fully_vaccinated=data['people_fully_vaccinated'],
+        date=data['date'],
+        source_url=data['source_url'],
+        vaccine=data['vaccine']
     )
 
 
