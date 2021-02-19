5296d448498afb27df82f1eb0964908eeb0e88d1
Edouard Mathieu <edmat@pm.me>
2021-02-19 08:54:08 +0100

    US/tests: calculate PR based on CDC cases

--- scripts/scripts/testing/automations/batch/united_states_hhs.R
+++ scripts/scripts/testing/automations/batch/united_states_hhs.R
@@ -2,34 +2,29 @@
 meta <- fromJSON(file = "https://healthdata.gov/api/3/action/package_show?id=c13c00e3-f3d0-4d49-8c43-bf600a6c0a0d")
 url <- meta$result[[1]]$resources[[1]]$url
 
-df <- fread(url, showProgress = FALSE, select = c("date", "new_results_reported"))
-setnames(df, "date", "Date")
+tests <- fread(url, showProgress = FALSE, select = c("date", "new_results_reported"))
+setnames(tests, "date", "Date")
 
-df <- df[, .(`Daily change in cumulative total` = sum(new_results_reported)), Date]
-setorder(df, Date)
+tests <- tests[, .(`Daily change in cumulative total` = sum(new_results_reported)), Date]
+tests[, Date := ymd(Date)]
+
+# Case counts
+cases <- jsonlite::fromJSON(txt = "https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=us_trend_data")$us_trend_data
+setDT(cases)
+cases <- cases[state == "United States", c("seven_day_avg_new_cases", "date")]
+setnames(cases, "date", "Date")
+cases[, Date := mdy(Date)]
 
 # Positive rate
-url <- read_html("https://www.cdc.gov/coronavirus/2019-ncov/covid-data/covidview/index.html") %>%
-    html_nodes(".syndicate a") %>%
-    html_attr("href") %>%
-    str_subset("csv$") %>%
-    paste0("https://www.cdc.gov", .)
-positive_rate <- fread(url, skip = 6, select = c("Week", "% of Specimens Positive for SARS-CoV-2"))
-positive_rate[, year := str_sub(Week, 1, 4)]
-positive_rate[, week_no := as.integer(str_sub(Week, 5, 6))]
-positive_rate[, Date := ymd(sprintf("%s0101", year))]
-week(positive_rate$Date) <- positive_rate$week_no
-positive_rate[, Date := Date + 3]
-positive_rate[, c("year", "week_no", "Week") := NULL]
-setnames(positive_rate, "% of Specimens Positive for SARS-CoV-2", "Positive rate")
-positive_rate[, `Positive rate` := round(`Positive rate` / 100, 3)]
-
-df <- merge(df, positive_rate, by = "Date", all.x = TRUE)
+df <- merge(tests, cases, by = "Date", all.x = TRUE)
+setorder(df, Date)
+df[, `Positive rate` := round(seven_day_avg_new_cases / frollmean(`Daily change in cumulative total`, 7), 3)]
+df[, seven_day_avg_new_cases := NULL]
 
 df[, Country := "United States"]
 df[, Units := "tests performed"]
 df[, `Source label` := "Department of Health & Human Services"]
-df[, `Source URL` := url]
+df[, `Source URL` := "https://healthdata.gov/dataset/covid-19-diagnostic-laboratory-testing-pcr-testing-time-series"]
 df[, Notes := NA_character_]
 
 fwrite(df, "automated_sheets/United States - tests performed.csv")
