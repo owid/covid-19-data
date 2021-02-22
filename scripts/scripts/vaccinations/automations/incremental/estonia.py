cb23e3f21610c2109338b72b5033cd3f80d50877
covid19owid <covid19owid@gmail.com>
2021-02-21 20:19:05 -0500

    Refactoring the pipeline for Estonia

--- scripts/scripts/vaccinations/automations/incremental/estonia.py
+++ scripts/scripts/vaccinations/automations/incremental/estonia.py
@@ -1,14 +1,16 @@
 import re
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 import vaxutils
 
 
-def main():
+def read(source: str) -> pd.Series:
+    soup = BeautifulSoup(requests.get(source).content, "html.parser")
+    return parse_data(soup)
 
-    url = "https://www.terviseamet.ee/et/uudised"
-    soup = BeautifulSoup(requests.get(url).content, "html.parser")
 
+def parse_data(soup: BeautifulSoup) -> pd.Series:
     for h2 in soup.find_all("h2", class_="views-field-title"):
         if "COVID-19 blogi" in h2.text:
             url = "https://www.terviseamet.ee" + h2.find("a")["href"]
@@ -16,28 +18,69 @@ def main():
 
     soup = BeautifulSoup(requests.get(url).content, "html.parser")
 
-    text = soup.find(class_="node-published").text
+    keys = ("date", "people_vaccinated", "people_fully_vaccinated", "source_url")
+    values = (parse_date(soup), parse_people_vaccinated(soup), parse_people_fully_vaccinated(soup), url)
+    data = dict(zip(keys, values))
+    return pd.Series(data=data)
 
-    people_vaccinated, people_fully_vaccinated = re.search(
-        r"COVID-19 vastu vaktsineerimisi on tehtud ([\d\s]+) inimesele, kellest ([\d\s]+) inimest on saanud mõlemad doosid",
-        text
-    ).groups()
 
-    people_vaccinated = vaxutils.clean_count(people_vaccinated)
-    people_fully_vaccinated = vaxutils.clean_count(people_fully_vaccinated)
-    total_vaccinations = people_vaccinated + people_fully_vaccinated
-
+def parse_date(soup: BeautifulSoup) -> str:
     date = soup.find(class_="field-name-post-date").text
     date = vaxutils.clean_date(date, "%d.%m.%Y")
+    return date
 
+
+def parse_people_vaccinated(soup: BeautifulSoup) -> int:
+    text = soup.find(class_="node-published").text
+    people_vaccinated = re.search(
+        r"Eestis on COVID-19 vastu vaktsineerimisi tehtud ([\d\s]+) inimesele, kaks doosi on saanud ([\d\s]+) inimest",
+        text
+    ).group(1)
+    return vaxutils.clean_count(people_vaccinated)
+
+
+def parse_people_fully_vaccinated(soup: BeautifulSoup) -> int:
+    text = soup.find(class_="node-published").text
+    people_fully_vaccinated = re.search(
+        r"Eestis on COVID-19 vastu vaktsineerimisi tehtud ([\d\s]+) inimesele, kaks doosi on saanud ([\d\s]+) inimest",
+        text
+    ).group(2)
+    return vaxutils.clean_count(people_fully_vaccinated)
+
+
+def add_totals(ds: pd.Series) -> pd.Series:
+    total_vaccinations = ds['people_vaccinated'] + ds['people_fully_vaccinated']
+    return vaxutils.enrich_data(ds, 'total_vaccinations', total_vaccinations)
+
+
+def enrich_location(ds: pd.Series) -> pd.Series:
+    return vaxutils.enrich_data(ds, 'location', "Estonia")
+
+
+def enrich_vaccine(ds: pd.Series) -> pd.Series:
+    return vaxutils.enrich_data(ds, 'vaccine', "Moderna, Oxford/AstraZeneca, Pfizer/BioNTech")
+
+
+def pipeline(ds: pd.Series) -> pd.Series:
+    return (
+        ds.pipe(add_totals)
+            .pipe(enrich_location)
+            .pipe(enrich_vaccine)
+
+    )
+
+
+def main():
+    source = "https://www.terviseamet.ee/et/uudised"
+    data = read(source).pipe(pipeline)
     vaxutils.increment(
-        location="Estonia",
-        total_vaccinations=total_vaccinations,
-        people_vaccinated=people_vaccinated,
-        people_fully_vaccinated=people_fully_vaccinated,
-        date=date,
-        source_url=url,
-        vaccine="Moderna, Oxford/AstraZeneca, Pfizer/BioNTech"
+        location=data['location'],
+        total_vaccinations=data['total_vaccinations'],
+        people_vaccinated=data['people_vaccinated'],
+        people_fully_vaccinated=data['people_fully_vaccinated'],
+        date=data['date'],
+        source_url=data['source_url'],
+        vaccine=data['vaccine']
     )
 
 
