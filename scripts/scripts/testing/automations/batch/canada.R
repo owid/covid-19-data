4ea95f4a46bdd59c9571c069053a1300675a5071
Cam Appel <c.appel@columbia.edu>
2021-03-03 13:13:57 -0500

    Update canada.R
    Add 'Canada - tests performed'

--- scripts/scripts/testing/automations/batch/canada.R
+++ scripts/scripts/testing/automations/batch/canada.R
@@ -1,18 +1,29 @@
 url <- "https://health-infobase.canada.ca/src/data/covidLive/covid19-download.csv"
 
-df <- fread(url, showProgress = FALSE, select = c("prname", "date", "numtested"))
+df <- fread(url, showProgress = FALSE, select = c("prname", "date", "numtested", "numtests"))
 df <- df[prname == "Canada"]
-df <- df[!is.na(numtested)]
 
-setnames(df, c("prname", "date", "numtested"), c("Country", "Date", "Cumulative total"))
-
-df[, Units := "people tested"]
 df[, `Source label` := "Government of Canada"]
 df[, `Source URL` := url]
-df[, `Testing type` := "PCR only"]
+df[, `Testing type` := "PCR + antigen"]
 df[, Notes := NA_character_]
 
-setorder(df, Date)
-df <- df[, .SD[1], `Cumulative total`]
+people <- copy(df)
+people[, Units := "people tested"]
+people[, numtests := NULL]
+setnames(people, c("prname", "date", "numtested"), c("Country", "Date", "Cumulative total"))
+setorder(people, Date)
+people <- people[!is.na(`Cumulative total`)]
+people <- people[, .SD[1], `Cumulative total`]
 
-fwrite(df, "automated_sheets/Canada.csv")
+fwrite(people, "automated_sheets/Canada - people tested.csv")
+
+tests <- copy(df)
+tests[, Units := "tests performed"]
+tests[, numtested := NULL]
+setnames(tests, c("prname", "date", "numtests"), c("Country", "Date", "Cumulative total"))
+setorder(tests, Date)
+tests <- tests[!is.na(`Cumulative total`)]
+tests <- tests[, .SD[1], `Cumulative total`]
+
+fwrite(tests, "automated_sheets/Canada - tests performed.csv")
