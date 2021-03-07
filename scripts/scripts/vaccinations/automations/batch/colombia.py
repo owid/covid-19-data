11aee53f21f0dc51a566274d2c5a97a52bfb8a03
Juan <57115669+juanpisuribe13@users.noreply.github.com>
2021-03-06 21:11:05 -0500

    Improved Text Reading and improved getImgText()
    by cropping the image, and setting it to gray and do some magic with opencv,
    since processimg gets it directly from the url, getimgtext has been replaced. refer to line 4

--- scripts/scripts/vaccinations/automations/batch/colombia.py
+++ scripts/scripts/vaccinations/automations/batch/colombia.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup
-import requests, pytesseract, pytz, sys, re
+import pytz, sys, re
+import requests, imutils, cv2, pytesseract
+from pytesseract import image_to_string as getImgText
+import numpy as np
 import pandas as pd
 from datetime import datetime
 try:
@@ -13,25 +16,34 @@ def getImageURL(hostname, path):
     soup = BeautifulSoup(url_request.text, features="lxml")
     return hostname + soup.find("div", {"class": "interSec1Lchikun left"}).find('img')['src']
 
-def getImgText(imgURL):
-    imgText = pytesseract.image_to_string(
-        Image.open(
-            requests.get(imgURL, stream=True).raw)
-    )
-    return imgText
+def processImg(url):
+    img = imutils.url_to_image(url)
+    img = img[299:389, 105:616] # crops the image to a specific position [y1:y2, x1:x2]
+                                # in this case, it's cropping the image to the amount of applied vaccines position
+    img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    img_gray, img_bin = cv2.threshold(img_gray,128,255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    img_gray = cv2.bitwise_not(img_bin)
+    kernel = np.ones((2, 1), np.uint8)
+    img = cv2.erode(img_gray, kernel, iterations=1)
+    img = cv2.dilate(img, kernel, iterations=1)
+    # credit to hucker marius for this portion of code. 
+    # this literally saved my life since imutils' tools weren't working the way i expected
+    # https://towardsdatascience.com/optical-character-recognition-ocr-with-less-than-12-lines-of-code-using-python-48404218cccb
+    # cv2.imwrite("img.jpeg", img)
+    return getImgText(img)
 
 def main():
     # Obtains both Image URL and text in Image via tesseract.
     hostname, path = "https://www.minsalud.gov.co", "/salud/publica/Vacunacion/Paginas/Vacunacion-covid-19.aspx"
     imgURL = getImageURL(hostname, path)
-    imgText = getImgText(imgURL)
 
-    # Gets the applied doses number 
-    b = imgText.find("DOSIS APLICADAS:")
-    appliedDoses = imgText[(b + 17):(b + 27)]
+    # Process Image
+    imgText = processImg(imgURL)
 
     # Eliminates all the involved strings if there's any
-    new_vaccinations = int(re.sub("[^\d]", "", appliedDoses))
+    # print(imgText)
+    new_vaccinations = int(re.sub("[^\d]", "", imgText))
+    # print("Vaccinated: %i" % (new_vaccinations))
 
     # Open CSV file
     df = pd.read_csv('Colombia.csv')
