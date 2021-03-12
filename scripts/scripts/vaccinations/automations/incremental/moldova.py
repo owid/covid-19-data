e4afd9015344d639225a618376faa60496577afc
Edouard Mathieu <edmat@pm.me>
2021-03-12 11:11:01 +0100

    Moldova/vax: use bs4 instead of selenium

--- scripts/scripts/vaccinations/automations/incremental/moldova.py
+++ scripts/scripts/vaccinations/automations/incremental/moldova.py
@@ -1,67 +1,71 @@
-import vaxutils
 import datetime
-import pytz
-import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+import requests
 import time
+
+from bs4 import BeautifulSoup
+import pandas as pd
+import pytz
+
+import vaxutils
 
 
 def read(source: str) -> pd.Series:
-    return connect_parse_data(source)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:86.0) Gecko/20100101 Firefox/86.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "Pragma": "no-cache",
+        "Cache-Control": "no-cache",
+    }
+    soup = BeautifulSoup(requests.get(source, headers=headers).content, "html.parser")
+    return parse_data(soup)
 
 
-def connect_parse_data(source: str) -> pd.Series:
-    op = Options()
-    op.add_argument("--headless")
+def parse_data(soup: BeautifulSoup) -> pd.Series:
 
-    with webdriver.Chrome(options=op) as driver:
-        driver.get(source)
-        time.sleep(5)
+    total_vaccinations = int(soup.find(id="stats").find_all("span")[0].text)
+    people_vaccinated = int(soup.find(id="stats").find_all("span")[1].text)
+    assert total_vaccinations >= people_vaccinated
 
-        for elem in driver.find_elements_by_class_name("col-lg-12"):
-            if "TOTAL DOZE ADMINISTRATE" in elem.text:
-                parent_elem = elem.parent
-                total_vaccinations = parent_elem.find_element_by_tag_name("span").text
-            if "PERSOANE VACCINATE" in elem.text:
-                parent_elem = elem.parent
-                people_vaccinated = parent_elem.find_element_by_tag_name("span").text
-
-        data = {'people_vaccinated': vaxutils.clean_count(people_vaccinated),
-                'total_vaccinations': vaxutils.clean_count(total_vaccinations)
-                }
+    data = {
+        "people_vaccinated": people_vaccinated,
+        "total_vaccinations": total_vaccinations,
+    }
     return pd.Series(data=data)
 
 
 def add_totals(ds: pd.Series) -> pd.Series:
-    people_fully_vaccinated = ds['total_vaccinations'] - ds['people_vaccinated']
-    return vaxutils.enrich_data(ds, 'people_fully_vaccinated', people_fully_vaccinated)
+    people_fully_vaccinated = ds["total_vaccinations"] - ds["people_vaccinated"]
+    return vaxutils.enrich_data(ds, "people_fully_vaccinated", people_fully_vaccinated)
 
 
 def format_date(ds: pd.Series) -> pd.Series:
     date = str(datetime.datetime.now(pytz.timezone("Europe/Chisinau")).date())
-    return vaxutils.enrich_data(ds, 'date', date)
+    return vaxutils.enrich_data(ds, "date", date)
 
 
 def enrich_location(ds: pd.Series) -> pd.Series:
-    return vaxutils.enrich_data(ds, 'location', "Moldova")
+    return vaxutils.enrich_data(ds, "location", "Moldova")
 
 
 def enrich_vaccine(ds: pd.Series) -> pd.Series:
-    return vaxutils.enrich_data(ds, 'vaccine', "Oxford/AstraZeneca")
+    return vaxutils.enrich_data(ds, "vaccine", "Oxford/AstraZeneca")
 
 
 def enrich_source(ds: pd.Series) -> pd.Series:
-    return vaxutils.enrich_data(ds, 'source_url', "https://vaccinare.gov.md/")
+    return vaxutils.enrich_data(ds, "source_url", "https://vaccinare.gov.md/")
 
 
 def pipeline(ds: pd.Series) -> pd.Series:
     return (
-        ds.pipe(add_totals)
-            .pipe(format_date)
-            .pipe(enrich_location)
-            .pipe(enrich_vaccine)
-            .pipe(enrich_source)
+        ds
+        .pipe(add_totals)
+        .pipe(format_date)
+        .pipe(enrich_location)
+        .pipe(enrich_vaccine)
+        .pipe(enrich_source)
     )
 
 
@@ -69,13 +73,13 @@ def main():
     source = "https://vaccinare.gov.md/"
     data = read(source).pipe(pipeline)
     vaxutils.increment(
-        location=data['location'],
-        total_vaccinations=data['total_vaccinations'],
-        people_vaccinated=data['people_vaccinated'],
-        people_fully_vaccinated=data['people_fully_vaccinated'],
-        date=data['date'],
-        source_url=data['source_url'],
-        vaccine=data['vaccine']
+        location=data["location"],
+        total_vaccinations=data["total_vaccinations"],
+        people_vaccinated=data["people_vaccinated"],
+        people_fully_vaccinated=data["people_fully_vaccinated"],
+        date=data["date"],
+        source_url=data["source_url"],
+        vaccine=data["vaccine"]
     )
 
 
