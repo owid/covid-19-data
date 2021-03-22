2b58e8b28efdad2b9c2dd2ed0af433a688c21bce
Edouard Mathieu <edmat@pm.me>
2021-03-22 15:32:16 +0100

    El Salvador/tests: refactor
    I've refactored the code so that on execution, the script only looks for new data (starting from the maximum date previously collected)

--- scripts/scripts/testing/automations/batch/el_salvador.py
+++ scripts/scripts/testing/automations/batch/el_salvador.py
@@ -1,39 +1,49 @@
-import re
+import datetime
 import requests
-from datetime import datetime, timedelta
+
+from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
-from bs4 import BeautifulSoup
 
 data = pd.read_csv("automated_sheets/El Salvador.csv")
 
-date = datetime(2020, 3, 16)
-enddate = datetime.today()
+date = pd.to_datetime(data.Date.max()).date()
+enddate = datetime.date.today()
 
 while date < enddate:
 
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
-    source_url = "https://diario.innovacion.gob.sv/?_token=PuE3sMLbt2Yu5VnljNa3scEuFYew5wp4VEBHcww2&fechaMostrar="+date.strftime('%d-%m-%Y')
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
+    }
+    source_url = f"https://diario.innovacion.gob.sv/?fechaMostrar={date.strftime('%d-%m-%Y')}"
 
-    req = requests.get(source_url, headers=headers)
-    soup = BeautifulSoup(req.content, 'html.parser')
+    try:
+        req = requests.get(source_url, headers=headers, timeout=10)
+    except Exception as e:
+        req = requests.get(source_url, headers=headers, timeout=10)
 
-    daily = soup.find('div', class_='col-4 col-sm-2 col-lg-2 align-self-center offset-lg-0').find('label').text.strip()
+    soup = BeautifulSoup(req.content, "html.parser")
+
+    daily = pd.to_numeric(
+        soup
+        .find("div", class_="col-4 col-sm-2 col-lg-2 align-self-center offset-lg-0")
+        .find("label")
+        .text.strip()
+    )
 
     new = pd.DataFrame({
-        "Date": [date],
+        "Date": [str(date)],
         "Daily change in cumulative total": daily,
-        "Country": 'El Salvador',
-        "Units": 'tests performed',
-        "Source_label": 'Government of El Salvador',
-        "Source_URL": source_url
-        })
+        "Country": "El Salvador",
+        "Units": "tests performed",
+        "Source label": "Government of El Salvador",
+        "Source URL": source_url
+    })
 
-    data = pd.concat([new, data], sort=False)
-    
-    date+=timedelta(days=1)
+    # print(f"{date} - {daily}")
 
-data['Daily change in cumulative total'].replace(['0'],[''], inplace=True)
-data['Daily change in cumulative total'].replace('', np.nan, inplace=True)
-data.dropna(subset = ["Daily change in cumulative total"], inplace=True)
-data.to_csv("automated_sheets/El Salvador.csv", index = False)
+    data = pd.concat([data, new])
+    data = data[data["Daily change in cumulative total"].fillna(0) > 0]
+    data.drop_duplicates().to_csv("automated_sheets/El Salvador.csv", index=False)
+
+    date += datetime.timedelta(days=1)
