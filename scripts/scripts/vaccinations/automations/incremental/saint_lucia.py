a8d57b845714b802aa781d5b2d9365bb6b88b436
Edouard Mathieu <edmat@pm.me>
2021-03-29 16:29:10 +0200

    Saint Lucia/vax: fix formatting

--- scripts/scripts/vaccinations/automations/incremental/saint_lucia.py
+++ scripts/scripts/vaccinations/automations/incremental/saint_lucia.py
@@ -1,13 +1,13 @@
 import datetime
 import re
+import time
 
-import pytz
-
-import vaxutils
 import pandas as pd
+import pytz
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import time
+
+import vaxutils
 
 
 def read(source: str) -> pd.Series:
@@ -31,34 +31,37 @@ def connect_parse_data(source: str) -> pd.Series:
             date = vaxutils.clean_date(date, "%B %d, %Y")
 
     people_vaccinated = arr[0]
-    data = {'people_vaccinated': vaxutils.clean_count(people_vaccinated),
-            'date': date}
+    data = {
+        "people_vaccinated": vaxutils.clean_count(people_vaccinated),
+        "date": date,
+    }
     return pd.Series(data=data)
 
 
 def add_totals(ds: pd.Series) -> pd.Series:
-    total_vaccinations = ds['people_vaccinated']
-    return vaxutils.enrich_data(ds, 'total_vaccinations', total_vaccinations)
+    total_vaccinations = ds["people_vaccinated"]
+    return vaxutils.enrich_data(ds, "total_vaccinations", total_vaccinations)
 
 
 def enrich_location(ds: pd.Series) -> pd.Series:
-    return vaxutils.enrich_data(ds, 'location', "Saint Lucia")
+    return vaxutils.enrich_data(ds, "location", "Saint Lucia")
 
 
 def enrich_vaccine(ds: pd.Series) -> pd.Series:
-    return vaxutils.enrich_data(ds, 'vaccine', "Oxford/AstraZeneca")
+    return vaxutils.enrich_data(ds, "vaccine", "Oxford/AstraZeneca")
 
 
 def enrich_source(ds: pd.Series) -> pd.Series:
-    return vaxutils.enrich_data(ds, 'source_url', "https://www.covid19response.lc/")
+    return vaxutils.enrich_data(ds, "source_url", "https://www.covid19response.lc/")
 
 
 def pipeline(ds: pd.Series) -> pd.Series:
     return (
-        ds.pipe(add_totals)
-            .pipe(enrich_location)
-            .pipe(enrich_vaccine)
-            .pipe(enrich_source)
+        ds
+        .pipe(add_totals)
+        .pipe(enrich_location)
+        .pipe(enrich_vaccine)
+        .pipe(enrich_source)
     )
 
 
@@ -66,12 +69,12 @@ def main():
     source = "https://www.covid19response.lc/"
     data = read(source).pipe(pipeline)
     vaxutils.increment(
-        location=data['location'],
-        total_vaccinations=data['total_vaccinations'],
-        people_vaccinated=data['people_vaccinated'],
-        date=data['date'],
-        source_url=data['source_url'],
-        vaccine=data['vaccine']
+        location=data["location"],
+        total_vaccinations=data["total_vaccinations"],
+        people_vaccinated=data["people_vaccinated"],
+        date=data["date"],
+        source_url=data["source_url"],
+        vaccine=data["vaccine"]
     )
 
 
