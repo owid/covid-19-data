c46c9911e81bd994707ac5611c30deb45c2db3dd
Cam Appel <camdappel@gmail.com>
2021-03-31 14:55:17 -0400

    Create Tunisia.R

---
+++ scripts/scripts/testing/automations/incremental/Tunisia.R
@@ -0,0 +1,21 @@
+url <- "https://onmne.tn"
+
+page <- read_html(url)
+
+values <- page %>%
+  html_nodes("span.vcex-milestone-time") %>%
+  .[[1]] %>%
+  html_attr("data-options") %>%
+  str_remove(fixed("{\"startVal\":0,\"endVal\":")) %>%
+  str_remove(fixed(",\"duration\":2,\"decimals\":0,\"separator\":\"\",\"decimal\":\".\"}")) %>%
+  as.integer()
+
+add_snapshot(
+  count = count,
+  sheet_name = "Tunisia",
+  country = "Tunisia",
+  units = "people tested",
+  source_url = url,
+  source_label = "Tunisia Minsitry of Health",
+  testing_type = "unclear"
+)
