b1efc3e95d510891c22365561d559c0ebd09dcd3
Cam Appel <camdappel@gmail.com>
2021-04-05 14:35:22 -0400

    Create liechtenstein.R

---
+++ scripts/scripts/testing/automations/batch/liechtenstein.R
@@ -0,0 +1,35 @@
+url <- read_html("https://www.covid19.admin.ch/fr/epidemiologic/test") %>%
+    html_nodes(".footer__nav .footer__nav__link") %>%
+    html_attr("href") %>%
+    str_subset("csv.zip") %>%
+    paste0("https://www.covid19.admin.ch", .)
+
+download.file(url, "tmp/liechtenstein.zip", quiet = TRUE)
+unzip("tmp/switzerland.zip", exdir = "tmp", files = "data/COVID19Test_geoRegion_PCR_Antigen.csv")
+
+df <- fread("tmp/data/COVID19Test_geoRegion_PCR_Antigen.csv",
+            select = c("datum", "entries", "entries_pos", "nachweismethode", "geoRegion"))
+df <- df[geoRegion == "FL"]
+
+df <- df[, .(
+    entries = sum(entries, na.rm = TRUE),
+    entries_pos = sum(entries_pos, na.rm = TRUE)
+), datum]
+
+setorder(df, datum)
+df[, pr := round(frollsum(entries_pos, 7) / frollsum(entries, 7), 3)]
+df <- df[, c("datum", "entries", "pr")]
+setnames(df, c("datum", "entries", "pr"), c("Date", "Daily change in cumulative total", "Positive rate"))
+
+df[, Country := "Liechtenstein"]
+df[, Units := "tests performed"]
+df[, `Source URL` := url]
+df[, `Source label` := "Federal Office of Public Health"]
+df[, Notes := NA_character_]
+
+df <- df[`Daily change in cumulative total` > 0]
+
+fwrite(df, "automated_sheets/Liechtenstein.csv")
+
+file.remove("tmp/data/COVID19Test_geoRegion_PCR_Antigen.csv")
+file.remove("tmp/liechtenstein.zip")
