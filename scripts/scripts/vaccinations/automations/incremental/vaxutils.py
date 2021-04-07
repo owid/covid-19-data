ab33945e27e11063a99d44b5f5c00477ce7c7252
lucas rg <lucasrg@kth.se>
2021-04-07 13:04:53 +0100

    Method to read xlsx safely from url

--- scripts/scripts/vaccinations/automations/incremental/vaxutils.py
+++ scripts/scripts/vaccinations/automations/incremental/vaxutils.py
@@ -1,6 +1,7 @@
 import datetime
 import re
 import requests
+import tempfile
 
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -10,13 +11,34 @@ def get_soup(source: str):
     headers = {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:86.0) Gecko/20100101 Firefox/86.0",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Encoding": "*",
         "Connection": "keep-alive",
         "Upgrade-Insecure-Requests": "1",
         "Pragma": "no-cache",
         "Cache-Control": "no-cache",
     }
     return BeautifulSoup(requests.get(source, headers=headers).content, "html.parser")
+
+
+def read_xlsx_from_url(url: str, as_series: bool) -> pd.DataFrame:
+    """Download and load xls file from URL.
+
+    Args:
+        url (str): File url.
+        as_series: Set to True to return a pandas.Series object. Source file must be of shape 1xN (1 row, N columns).
+
+    Returns:
+        pandas.DataFrame: Data loaded.
+    """
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux i686)"}
+    response = requests.get(url, headers=headers)
+    with tempfile.NamedTemporaryFile() as tmp:
+        with open(tmp.name, 'wb') as f:
+            f.write(response.content)
+        df = pd.read_excel(tmp.name)
+    if as_series:
+        return df.T.squeeze()
+    return df
 
 
 def clean_count(count):
