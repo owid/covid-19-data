f8a5830a7eac20e9d912a431bec908ca14e3f63e
lucas rg <lucasrg@kth.se>
2021-04-07 15:29:15 +0100

    San Marino has been using Pfizer/BioNTech

--- scripts/scripts/vaccinations/automations/incremental/san_marino.py
+++ scripts/scripts/vaccinations/automations/incremental/san_marino.py
@@ -48,7 +48,7 @@ def enrich_location(ds: pd.Series) -> pd.Series:
 
 
 def enrich_vaccine(ds: pd.Series) -> pd.Series:
-    return vaxutils.enrich_data(ds, "vaccine", "Sputnik V")
+    return vaxutils.enrich_data(ds, "vaccine", "Pfizer/BioNTech, Sputnik V")
 
 
 def enrich_source(ds: pd.Series) -> pd.Series:
