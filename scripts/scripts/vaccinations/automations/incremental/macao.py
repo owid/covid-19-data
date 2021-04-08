b9546285a40a80f393183137eb55ca9ee8b37f34
lucas rg <lucasrg@kth.se>
2021-04-08 22:27:20 +0100

    1st and 2nd doses added to Macao: Changes in Regex + CSV structure

--- scripts/scripts/vaccinations/automations/incremental/macao.py
+++ scripts/scripts/vaccinations/automations/incremental/macao.py
@@ -1,5 +1,6 @@
 import re
 from datetime import datetime
+import os
 
 import requests
 from bs4 import BeautifulSoup
@@ -17,8 +18,24 @@ def parse_source_url(elem) -> str:
     return elem.find_parent(class_="card").find("a").get("href")
 
 
-def parse_vaccinations(text: str, regex_pattern: str) -> int:
-    return vaxutils.clean_count(re.search(regex_pattern, text).group(1))
+def parse_vaccinations(elem) -> dict:
+    # Get news text
+    url = elem.find_parent(class_="card").find("a").get("href")
+    soup = vaxutils.get_soup(url)
+    text = "\n".join([p.text for p in soup.find("article").find_all("p")])
+
+    # Find metrics
+    metrics = dict()
+    total_vaccinations = re.search(r"疫苗共有(?P<count>[\d,]*)人次", text)
+    people_vaccinated = re.search(r"1劑疫苗共有(?P<count>[\d,]*)人次", text)
+    people_fully_vaccinated = re.search(r"2劑疫苗共有(?P<count>[\d,]*)人次", text)
+    if total_vaccinations:
+        metrics["total_vaccinations"] = vaxutils.clean_count(total_vaccinations.group(1))
+    if people_vaccinated:
+        metrics["people_vaccinated"] = vaxutils.clean_count(people_vaccinated.group(1))
+    if people_fully_vaccinated:
+        metrics["people_fully_vaccinated"] = vaxutils.clean_count(people_fully_vaccinated.group(1))
+    return metrics
 
 
 def parse_data(soup: BeautifulSoup) -> pd.Series:
@@ -26,62 +43,71 @@ def parse_data(soup: BeautifulSoup) -> pd.Series:
     # Get all h3 elements
     elems = soup.find_all("h3")
     # Get data
+    records = []
     for elem in elems:
-        text = elem.find(text=re.compile(regex_pattern))
-        if text:
-            return pd.Series({
+        if elem.find(text=re.compile(regex_pattern)):
+            records.append({
                 "date": parse_date(elem),
                 "source_url": parse_source_url(elem),
-                "total_vaccinations": parse_vaccinations(text, regex_pattern)
+                **parse_vaccinations(elem)
             })
-    return None
+    return records
 
 
-def read(source: str, num_pages_limit: int = 10):
-    # Load page
+def read(source: str, last_update: str, num_pages_limit: int = 10):
+    records = []
     for page_nr in range(1, num_pages_limit):
         # Get soup
         url = f"{source}/{page_nr}/"
         soup = vaxutils.get_soup(url)
-        # Get data
-        ds = parse_data(soup)
-        if ds is not None:
-            return ds
-    raise Exception("No news page with vaccination data was found. Check URLs.")
+        # Get data (if any)
+        records_sub = parse_data(soup)
+        if records_sub:
+            records.extend(records_sub)
+            if any([record["date"] <= last_update for record in records_sub]):
+                break
+    if len(records) > 0:
+        records = [record for record in records if record["date"] >= last_update]
+        if len(records) > 0:
+            return pd.DataFrame(records)
+    return None
 
 
-def enrich_location(ds: pd.Series) -> pd.Series:
-    return vaxutils.enrich_data(ds, "location", "Macao")
+def enrich_location(df: pd.DataFrame) -> pd.DataFrame:
+    return df.assign(location="Macao")
 
 
-def enrich_vaccine(ds: pd.Series) -> pd.Series:
-    return vaxutils.enrich_data(ds, "vaccine", "Sinopharm/Beijing, Pfizer/BioNTech")
+def enrich_vaccine(df: pd.DataFrame) -> pd.DataFrame:
+    return df.assign(vaccine="Sinopharm/Beijing, Pfizer/BioNTech")
 
 
-def add_totals(ds: pd.Series) -> pd.Series:
-    return vaxutils.enrich_data(ds, "people_vaccinated", ds.total_vaccinations)
-
-
-def pipeline(ds: pd.Series) -> pd.Series:
+def pipeline(df: pd.DataFrame) -> pd.DataFrame:
     return (
-        ds
-        .pipe(add_totals)
+        df
         .pipe(enrich_location)
         .pipe(enrich_vaccine)
     )
 
 
+def merge_with_current_data(df: pd.DataFrame, filepath: str) -> pd.DataFrame:
+    # Load current data
+    if os.path.isfile(filepath):
+        df_current = pd.read_csv(filepath)
+        #  Merge
+        df_current = df_current[~df_current.date.isin(df.date)]
+        df = pd.concat([df, df_current]).sort_values(by="date")
+    return df
+
+
 def main():
     source = "https://www.gov.mo/zh-hant/news/page"
-    data = read(source).pipe(pipeline)
-    vaxutils.increment(
-        location=data["location"],
-        total_vaccinations=data["total_vaccinations"],
-        people_vaccinated=data["people_vaccinated"],
-        date=data["date"],
-        source_url=data["source_url"],
-        vaccine=data["vaccine"]
-    )
+    output_file = "automations/output/Macao.csv"
+    last_update = pd.read_csv("automations/output/Macao.csv").date.max()
+    df = read(source, last_update)
+    if df is not None:
+        df = df.pipe(pipeline)
+        df = merge_with_current_data(df, output_file)
+        df.to_csv(output_file, index=False)
 
 
 if __name__ == "__main__":
