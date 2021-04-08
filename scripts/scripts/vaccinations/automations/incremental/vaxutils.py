de1e4cc4de176a8ee2003e699123853b49c32557
lucas rg <lucasrg@kth.se>
2021-04-08 11:06:03 +0100

    add kwargs to read_xlsx_from_url method

--- scripts/scripts/vaccinations/automations/incremental/vaxutils.py
+++ scripts/scripts/vaccinations/automations/incremental/vaxutils.py
@@ -22,12 +22,13 @@ def get_soup(source: str):
     return BeautifulSoup(requests.get(source, headers=headers).content, "html.parser")
 
 
-def read_xlsx_from_url(url: str, as_series: bool) -> pd.DataFrame:
+def read_xlsx_from_url(url: str, as_series: bool, **kwargs) -> pd.DataFrame:
     """Download and load xls file from URL.
 
     Args:
         url (str): File url.
         as_series: Set to True to return a pandas.Series object. Source file must be of shape 1xN (1 row, N columns).
+        kwargs: Arguments for pandas.read_excel.
 
     Returns:
         pandas.DataFrame: Data loaded.
@@ -37,7 +38,7 @@ def read_xlsx_from_url(url: str, as_series: bool) -> pd.DataFrame:
     with tempfile.NamedTemporaryFile() as tmp:
         with open(tmp.name, 'wb') as f:
             f.write(response.content)
-        df = pd.read_excel(tmp.name)
+        df = pd.read_excel(tmp.name, **kwargs)
     if as_series:
         return df.T.squeeze()
     return df
