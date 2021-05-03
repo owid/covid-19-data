050b4ee35864ad58f0f29127584f438b7ff2d6de
covid19owid <covid19owid@gmail.com>
2021-05-02 20:04:05 -0400

    Update qatar.py
    Qatar : Update vaccine list

--- scripts/scripts/vaccinations/src/vax/incremental/qatar.py
+++ scripts/scripts/vaccinations/src/vax/incremental/qatar.py
@@ -7,11 +7,11 @@ from selenium.webdriver.chrome.options import Options
 from vax.utils.incremental import enrich_data, increment, clean_date, clean_count
 
 
-def read(source: str) -> pd.Series:
-    return connect_parse_data(source)
+def read(source: str, source_old: str) -> pd.Series:
+    return connect_parse_data(source, source_old)
 
 
-def connect_parse_data(source: str) -> pd.Series:
+def connect_parse_data(source: str, source_old: str) -> pd.Series:
     op = Options()
     op.add_argument("--headless")
 
@@ -20,12 +20,24 @@ def connect_parse_data(source: str) -> pd.Series:
         time.sleep(5)
 
         total_vaccinations = driver.find_element_by_id("counter1").text
+        people_vaccinated = driver.find_element_by_id("counter2").text
+        people_fully_vaccinated = driver.find_element_by_id("counter3").text
+        
+        driver.get(source_old)
+        time.sleep(5)
 
+        # Sanity check
+        total_vaccinations_old = driver.find_element_by_id("counter1").text
+        if total_vaccinations != total_vaccinations_old:
+            raise ValueError("Both dashboards may not be synced and hence may refer to different timestamps. Consider"
+                             "Introducing the timestamp manually.")
         date = driver.find_element_by_id("pupdateddate").text
         date = clean_date(date.replace("Updated ", ""), "%d %b, %Y")
 
     data = {
         "total_vaccinations": clean_count(total_vaccinations),
+        "people_vaccinated": clean_count(people_vaccinated),
+        "people_fully_vaccinated": clean_count(people_fully_vaccinated),
         "date": date,
     }
     return pd.Series(data=data)
@@ -36,7 +48,7 @@ def enrich_location(ds: pd.Series) -> pd.Series:
 
 
 def enrich_vaccine(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, "vaccine", "Pfizer/BioNTech")
+    return enrich_data(ds, "vaccine", "Moderna, Pfizer/BioNTech")
 
 
 def enrich_source(ds: pd.Series, source: str) -> pd.Series:
@@ -53,11 +65,14 @@ def pipeline(ds: pd.Series, source: str) -> pd.Series:
 
 
 def main():
-    source = "https://covid19.moph.gov.qa/EN/Pages/default.aspx"
-    data = read(source).pipe(pipeline, source)
+    source = "https://covid19.moph.gov.qa/EN/Pages/Vaccination-Program-Data.aspx"
+    source_old = "https://covid19.moph.gov.qa/EN/Pages/default.aspx"
+    data = read(source, source_old).pipe(pipeline, source)
     increment(
         location=data["location"],
         total_vaccinations=data["total_vaccinations"],
+        people_vaccinated=data["people_vaccinated"],
+        people_fully_vaccinated=data["people_fully_vaccinated"],
         date=data["date"],
         source_url=data["source_url"],
         vaccine=data["vaccine"]
