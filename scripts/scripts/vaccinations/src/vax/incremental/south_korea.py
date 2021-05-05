e02394c7aff862d5bde54928bb06074694ce049d
covid19owid <covid19owid@gmail.com>
2021-05-05 08:33:25 -0400

    Update south_korea.py
    Update vaccine list

--- scripts/scripts/vaccinations/src/vax/incremental/south_korea.py
+++ scripts/scripts/vaccinations/src/vax/incremental/south_korea.py
@@ -49,7 +49,7 @@ def enrich_location(ds: pd.Series) -> pd.Series:
 
 
 def enrich_vaccine(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, "vaccine", "Johnson&Johnson, Oxford/AstraZeneca, Pfizer/BioNTech")
+    return enrich_data(ds, "vaccine", "Oxford/AstraZeneca, Pfizer/BioNTech")
 
 
 def enrich_source(ds: pd.Series) -> pd.Series:
