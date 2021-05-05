a2187cd6add8eb709373d23ebe2216883b5f7d4d
covid19owid <covid19owid@gmail.com>
2021-05-05 08:32:56 -0400

    Update colombia.py
    Update vaccine list

--- scripts/scripts/vaccinations/src/vax/incremental/colombia.py
+++ scripts/scripts/vaccinations/src/vax/incremental/colombia.py
@@ -43,7 +43,7 @@ def enrich_location(ds: pd.Series) -> pd.Series:
 
 
 def enrich_vaccine(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, "vaccine", "Johnson&Johnson, Oxford/AstraZeneca, Pfizer/BioNTech, Sinovac")
+    return enrich_data(ds, "vaccine", "Oxford/AstraZeneca, Pfizer/BioNTech, Sinovac")
 
 
 def enrich_source(ds: pd.Series, source: str) -> pd.Series:
