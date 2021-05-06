0346bee5b64e4ac36e5ee34951f5ed9614f2485a
lucas rg <lucasrg@kth.se>
2021-05-06 18:55:21 +0100

    Fix Poland, switch to new API

--- scripts/scripts/vaccinations/src/vax/incremental/poland.py
+++ scripts/scripts/vaccinations/src/vax/incremental/poland.py
@@ -1,79 +1,88 @@
 import json
+from datetime import datetime, timedelta
+
 import requests
-from vax.utils.incremental import enrich_data, increment
 import pandas as pd
 
-
-def read(source: str) -> pd.Series:
-    data = json.loads(requests.get(source).content)["features"][0]["attributes"]
-    return parse_data(data)
+from vax.utils.incremental import enrich_data, increment
+from vax.utils.files import load_query
 
 
-def parse_data(data: dict) -> pd.Series:
-    keys = ("date", "total_vaccinations", "people_fully_vaccinated")
-    values = (parse_date(data), parse_total_vaccinations(data), parse_people_fully_vaccinated(data))
-    data = dict(zip(keys, values))
-    return pd.Series(data=data)
+class Poland:
 
+    def __init__(self, source_url: str, source_url_ref: str, location: str, columns_rename: dict = None):
+        self.source_url = source_url
+        self.source_url_ref = source_url_ref
+        self.location = location
+        self.columns_rename = columns_rename
 
-def parse_date(data: dict) -> str:
-    date = data["Data"]
-    date = str((pd.to_datetime(date, unit="ms").date() - pd.DateOffset(days=1)).date())
-    return date
+    @property
+    def output_file(self):
+        return f"./output/{self.location}.csv"
 
+    def read(self) -> pd.Series:
+        params = load_query('poland-all', to_str=False)
+        data = requests.get(self.source_url, params=params).json()["features"][0]["attributes"]
+        return pd.Series(data)
 
-def parse_total_vaccinations(data: dict) -> int:
-    return int(data["SZCZEPIENIA_SUMA"])
+    def pipe_rename_columns(self, ds: pd.Series) -> pd.Series:
+        return ds.rename(self.columns_rename)
+    
+    def pipe_date(self, ds: pd.Series) -> pd.Series:
+        ds.loc["date"] = (
+            (datetime.fromtimestamp(ds.date/1000) - timedelta(days=1))
+            .strftime("%Y-%m-%d")
+        )
+        return ds
 
+    def pipe_location(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(ds, 'location', self.location)
 
-def parse_people_fully_vaccinated(data: dict) -> int:
-    return int(data["DAWKA_2_SUMA"])
+    def pipe_vaccine(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(ds, 'vaccine', "Johnson&Johnson, Moderna, Oxford/AstraZeneca, Pfizer/BioNTech")
 
+    def pipe_source(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(ds, 'source_url', self.source_url_ref)
 
-def add_totals(ds: pd.Series) -> pd.Series:
-    people_vaccinated = ds['total_vaccinations'] - ds['people_fully_vaccinated']
-    return enrich_data(ds, 'people_vaccinated', people_vaccinated)
+    def pipeline(self, ds: pd.Series) -> pd.Series:
+        return (
+            ds
+            .pipe(self.pipe_rename_columns)
+            .pipe(self.pipe_date)
+            .pipe(self.pipe_location)
+            .pipe(self.pipe_vaccine)
+            .pipe(self.pipe_source)
+        )
 
-
-def enrich_location(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, 'location', "Poland")
-
-
-def enrich_vaccine(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, 'vaccine', "Johnson&Johnson, Moderna, Oxford/AstraZeneca, Pfizer/BioNTech")
-
-
-def enrich_source(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, 'source_url', "https://www.gov.pl/web/szczepimysie/raport-szczepien-przeciwko-covid-19")
-
-
-def pipeline(ds: pd.Series) -> pd.Series:
-    return (
-        ds
-        .pipe(add_totals)
-        .pipe(enrich_location)
-        .pipe(enrich_vaccine)
-        .pipe(enrich_source)
-    )
+    def to_csv(self, output_file: str = None):
+        """Generalized."""
+        data = self.read().pipe(self.pipeline)
+        increment(
+            location=data['location'],
+            total_vaccinations=data['total_vaccinations'],
+            people_vaccinated=data['people_vaccinated'],
+            people_fully_vaccinated=data['people_fully_vaccinated'],
+            date=data['date'],
+            source_url=data['source_url'],
+            vaccine=data['vaccine']
+        )
 
 
 def main():
-    source = (
-        "https://services-eu1.arcgis.com/zk7YlClTgerl62BY/arcgis/rest/services/global_szczepienia_widok2/"
-        "FeatureServer/0/query?f=json&where=Data%20BETWEEN%20(CURRENT_TIMESTAMP%20-%20INTERVAL%20%2724%27%20HOUR)"
-        "%20AND%20CURRENT_TIMESTAMP&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&resultOffset="
-        "0&resultRecordCount=1&resultType=standard"
-    )
-    data = read(source).pipe(pipeline)
-    increment(
-        location=data['location'],
-        total_vaccinations=data['total_vaccinations'],
-        people_vaccinated=data['people_vaccinated'],
-        people_fully_vaccinated=data['people_fully_vaccinated'],
-        date=data['date'],
-        source_url=data['source_url'],
-        vaccine=data['vaccine']
-    )
+    Poland(
+        source_url=(
+            "https://services-eu1.arcgis.com/zk7YlClTgerl62BY/ArcGIS/rest/services/global_szczepienia_actual_widok3/"
+            "FeatureServer/0/query"
+        ),
+        source_url_ref="https://www.gov.pl/web/szczepimysie/raport-szczepien-przeciwko-covid-19",
+        location="Poland",
+        columns_rename={
+            "SZCZEPIENIA_SUMA": "total_vaccinations",
+            "DAWKA_1_SUMA": "people_vaccinated",
+            "zaszczepieni_finalnie": "people_fully_vaccinated",
+            "Data": "date"
+        }
+    ).to_csv() 
 
 
 if __name__ == "__main__":
